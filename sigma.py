959a0af9a0bbfd56c8461c73cb9b0d0e094f05c4
awaken1ng <lucidsunlight@yandex.ru>
2016-08-20 18:37:19 +0700

    Revert "Attemp to move the OW API call to function"
    This reverts commit 4d048e6061b13bcaf7701d11354240b7755a87c1.

--- sigma.py
+++ sigma.py
@@ -10,66 +10,6 @@ import urllib.error
 import wget
 from PIL import Image
 
-def RequstOWProfile(initiator_data, ow_name):
-    cmd_name = 'Overwatch'
-    profile = ('https://api.lootbox.eu/pc/eu/' + ow_name + '/profile')
-    profile_json_source = urllib.request.urlopen(profile).read().decode('utf-8')
-    profile_json = json.loads(profile_json_source)
-    try:
-        avatar_link = profile_json['data']['avatar']
-        border_link = profile_json['data']['levelFrame']
-        if os.path.isfile('avatar.png') == True:
-            os.remove('avatar.png')
-        if os.path.isfile('border.png') == True:
-            os.remove('border.png')
-        if os.path.isfile('profile.png') == True:
-            os.remove('profile.png')
-        wget.download(avatar_link)
-        avatar_link_base = 'https://blzgdapipro-a.akamaihd.net/game/unlocks/'
-        avatar_name = str(profile_json['data']['avatar'])
-        os.rename(avatar_name[len(avatar_link_base):], 'avatar.png')
-        wget.download(border_link)
-        border_link_base = 'https://blzgdapipro-a.akamaihd.net/game/playerlevelrewards/'
-        border_name = str(profile_json['data']['levelFrame'])
-        os.rename(border_name[len(border_link_base):], 'border.png')
-        base = Image.open('base.png')
-        foreground = Image.open('border.png')
-        foreground_res = foreground.resize((176, 176), Image.ANTIALIAS)
-        background = Image.open('avatar.png')
-        background_res = background.resize((72, 72), Image.ANTIALIAS)
-        base.paste(background_res, (28, 28), background_res)
-        base.paste(foreground_res, (-24, -24), foreground_res)
-        profile = base.save('profile.png') #base.save('profile.png')
-        overwatch_profile = ('**Name:** ' + profile_json['data']['username'] +
-                            '\n**Level:** ' + str(profile_json['data']['level']) +
-                            '\n**Quick Games:**' +
-                            '\n    **- Played:** ' + str(profile_json['data']['games']['quick']['played']) +
-                            '\n    **- Won:** ' + str(profile_json['data']['games']['quick']['wins']) +
-                            '\n    **- Lost:** ' + str(profile_json['data']['games']['quick']['lost']) +
-                            '\n**Competitive Games:**' +
-                            '\n    **- Played:** ' + str(profile_json['data']['games']['competitive']['played']) +
-                            '\n    **- Won:** ' + str(profile_json['data']['games']['competitive']['wins']) +
-                            '\n    **- Lost:** ' + str(profile_json['data']['games']['competitive']['lost']) +
-                            '\n    **- Rank:** ' + str(profile_json['data']['games']['competitive']['lost']) +
-                            '\n**Playtime:**' +
-                            '\n    **- Quick:** ' +str(profile_json['data']['playtime']['quick']) +
-                            '\n    **- Competitive:** ' + str(profile_json['data']['playtime']['competitive'])
-                             )
-        print('CMD [' + cmd_name + '] > ' + initiator_data)
-        return (True, overwatch_profile, profile)
-            #await client.send_file(message.channel, 'profile.png')
-            #await client.send_message(message.channel, overwatch_profile)
-    except KeyError:
-        try:
-            print('CMD [' + cmd_name + '] > ' + initiator_data)
-            print(profile_json['error'])
-            return (False, profile_json['error'])
-            #await client.send_message(message.channel, profile_json['error'])
-        except:
-            print('CMD [' + cmd_name + '] > ' + initiator_data)
-            return (False, 'Something went wrong.\nThe servers are most likely overloaded, please try again.')
-            #await client.send_message(message.channel, 'Something went wrong.\nThe servers are most likely overloaded, please try again.')
-
 print('Starting up...')
 start_time = time.time()
 time = time.time()
@@ -146,11 +86,58 @@ async def on_message(message):
         cmd_name = 'Overwatch'
         client.send_typing(message.channel)
         ow_name = (str(message.content[len(cmd_overwatch) + 1 + len(pfx):])).replace('#', '-')
-        response = RequstOWProfile(initiator_data, ow_name)
-        if (response[0]):
-            await client.send_file(message.channel, response[1])
-            await client.send_message(message.channel, response[2])
-        else: await client.send_message(message.channel, response[1])
-
-
+        profile = ('https://api.lootbox.eu/pc/eu/' + ow_name + '/profile')
+        profile_json_source = urllib.request.urlopen(profile).read().decode('utf-8')
+        profile_json = json.loads(profile_json_source)
+        try:
+            avatar_link = profile_json['data']['avatar']
+            border_link = profile_json['data']['levelFrame']
+            if os.path.isfile('avatar.png') == True:
+                os.remove('avatar.png')
+            if os.path.isfile('border.png') == True:
+                os.remove('border.png')
+            if os.path.isfile('profile.png') == True:
+                os.remove('profile.png')
+            wget.download(avatar_link)
+            avatar_link_base = 'https://blzgdapipro-a.akamaihd.net/game/unlocks/'
+            avatar_name = str(profile_json['data']['avatar'])
+            os.rename(avatar_name[len(avatar_link_base):], 'avatar.png')
+            wget.download(border_link)
+            border_link_base = 'https://blzgdapipro-a.akamaihd.net/game/playerlevelrewards/'
+            border_name = str(profile_json['data']['levelFrame'])
+            os.rename(border_name[len(border_link_base):], 'border.png')
+            base = Image.open('base.png')
+            foreground = Image.open('border.png')
+            foreground_res = foreground.resize((176, 176), Image.ANTIALIAS)
+            background = Image.open('avatar.png')
+            background_res = background.resize((72, 72), Image.ANTIALIAS)
+            base.paste(background_res, (28, 28), background_res)
+            base.paste(foreground_res, (-24, -24), foreground_res)
+            base.save('profile.png')
+            overwatch_profile = ('**Name:** ' + profile_json['data']['username'] +
+                                '\n**Level:** ' + str(profile_json['data']['level']) +
+                                '\n**Quick Games:**' +
+                                '\n    **- Played:** ' + str(profile_json['data']['games']['quick']['played']) +
+                                '\n    **- Won:** ' + str(profile_json['data']['games']['quick']['wins']) +
+                                '\n    **- Lost:** ' + str(profile_json['data']['games']['quick']['lost']) +
+                                '\n**Competitive Games:**' +
+                                '\n    **- Played:** ' + str(profile_json['data']['games']['competitive']['played']) +
+                                '\n    **- Won:** ' + str(profile_json['data']['games']['competitive']['wins']) +
+                                '\n    **- Lost:** ' + str(profile_json['data']['games']['competitive']['lost']) +
+                                '\n    **- Rank:** ' + str(profile_json['data']['games']['competitive']['lost']) +
+                                '\n**Playtime:**' +
+                                '\n    **- Quick:** ' +str(profile_json['data']['playtime']['quick']) +
+                                '\n    **- Competitive:** ' + str(profile_json['data']['playtime']['competitive'])
+                                 )
+            print('CMD [' + cmd_name + '] > ' + initiator_data)
+            await client.send_file(message.channel, 'profile.png')
+            await client.send_message(message.channel, overwatch_profile)
+        except KeyError:
+            try:
+                print('CMD [' + cmd_name + '] > ' + initiator_data)
+                print(profile_json['error'])
+                await client.send_message(message.channel, profile_json['error'])
+            except:
+                print('CMD [' + cmd_name + '] > ' + initiator_data)
+                await client.send_message(message.channel, 'Something went wrong.\nThe servers are most likely overloaded, please try again.')
 client.run(token)
