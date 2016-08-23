32b775ed0cd87252ad2e03b95d96e6f06b47f0ab
AXAz0r <thealeksaradovic@gmail.com>
2016-08-23 11:43:06 +0200

    STUFF ADDED!

--- sigma.py
+++ sigma.py
@@ -1,13 +1,15 @@
-import discord
-import asyncio
-import sys
-import os
-import time
+# noinspection PyPep8
 import datetime
 import json
-import urllib.request
+import os
+import sys
+import time
 import urllib.error
+import urllib.request
+import discord
+import lxml.html
 import wget
+import requests
 from PIL import Image
 
 print('Starting up...')
@@ -16,11 +18,11 @@ time = time.time()
 current_time = datetime.datetime.now().time()
 current_time.isoformat()
 
-if os.path.isfile('config.json') == False:
+if not os.path.isfile('config.json'):
     sys.exit('Fatal Error: config.json is not present.\nIf you didn\'t already, rename config.example.json to config.json and try again.')
 else:
     print('config.json present, continuing...')
-if os.path.isfile('commands.json') == False:
+if not os.path.isfile('commands.json'):
     sys.exit('Fatal Error: commands.json is not present.\nIf you didn\'t already, rename config.example.json to config.json and try again.')
 else:
     print('commands.json present, continuing...')
@@ -41,11 +43,16 @@ pfx = (config['Prefix'])
 ownr = (config['OwnerID'])
 client = discord.Client()
 riot_api_key = '759fa53e-7837-4109-bf6a-05b8dc63d702'
+mashape_key = 'nvLNoBix6DmshG97ORG4iB51mHa5p1UezKwjsnigQ85K5RXieT'
+owm_key = 'b49efc119530833da61588e4d87668c1'
 
 # Commands
 cmd_count = (commands['cmd_count'])
 cmd_overwatch = (commands['cmd_overwatch'])
 cmd_league = (commands['cmd_league'])
+cmd_bns = (commands['cmd_bns'])
+cmd_ud = (commands['cmd_ud'])
+cmd_weather = (commands['cmd_weather'])
 
 # I love spaghetti!
 
@@ -73,7 +80,6 @@ async def on_ready():
 async def on_message(message):
     # Static Strings
     initiator_data = ('by: ' + str(message.author) + '\nUserID: ' + str(message.author.id) + '\nServer: ' + str(message.server.name) + '\nServerID: ' + str(message.server.id) + '\n-----------------------------------------')
-    permission_error = ('PERMISSION DENIED!\nCommand user by: ' + str(message.author) + '\nUserID: ' + str(message.author.id) + '\nServer: ' + str(message.server.name) + '\nServerID: ' + str(message.server.id) + '\n-----------------------------------------')
     client.change_status(game=None)
     if message.content.startswith(pfx + cmd_count):
         cmd_name = 'Count'
@@ -90,30 +96,23 @@ async def on_message(message):
         await client.send_typing(message.channel)
         ow_input = (str(message.content[len(cmd_overwatch) + 1 + len(pfx):])).replace('#', '-')
         ow_region_x, ignore, ow_name = ow_input.partition(' ')
-        #ow_name = (str(ow_input[3:]))
-        #ow_region = (str(ow_input[:12]))
         ow_region = ow_region_x.replace('NA', 'US')
-        #profile = ('https://api.lootbox.eu/pc/eu/' + ow_name + '/profile')
         profile = ('http://127.0.0.1:9000/pc/' + ow_region.lower() + '/' + ow_name + '/profile').replace(' ', '')
         profile_json_source = urllib.request.urlopen(profile).read().decode('utf-8')
         profile_json = json.loads(profile_json_source)
         try:
             avatar_link = profile_json['data']['avatar']
             border_link = profile_json['data']['levelFrame']
-            if os.path.isfile('avatar.png') == True:
+            if os.path.isfile('avatar.png'):
                 os.remove('avatar.png')
-            if os.path.isfile('border.png') == True:
+            if os.path.isfile('border.png'):
                 os.remove('border.png')
-            if os.path.isfile('profile.png') == True:
+            if os.path.isfile('profile.png'):
                 os.remove('profile.png')
             wget.download(avatar_link)
             avatar_link_base = 'https://blzgdapipro-a.akamaihd.net/game/unlocks/'
             avatar_name = str(profile_json['data']['avatar'])
             os.rename(avatar_name[len(avatar_link_base):], 'avatar.png')
-            #avatar_pre_conv = Image.open('avatar.png')
-            #avatar_converted = Image.new("RGB", avatar_pre_conv.size, (255,255,255))
-            #avatar_converted.paste(avatar_pre_conv, (0, 0), avatar_pre_conv)
-            #avatar_converted.save('avatar.png')
             wget.download(border_link)
             border_link_base = 'https://blzgdapipro-a.akamaihd.net/game/playerlevelrewards/'
             border_name = str(profile_json['data']['levelFrame'])
@@ -132,7 +131,6 @@ async def on_message(message):
                 rank_error = 'Goddamn it Bubu!'
             else:
                 rank_error = 'Season not active.'
-            #Original Rank Code: str(profile_json['data']['games']['competitive']['lost']
             overwatch_profile = ('**Name:** ' + profile_json['data']['username'] +
                                 '\n**Level:** ' + str(profile_json['data']['level']) +
                                 '\n**Quick Games:**' +
@@ -163,52 +161,21 @@ async def on_message(message):
     elif message.content.startswith(pfx + cmd_league + ' '):
         await client.send_typing(message.channel)
         cmd_name = 'League of Legends'
-        lol_input = (str(message.content[len(cmd_league) + 1 + len(pfx):])).replace('#', '-')
+        if os.path.isfile('lolsig.png'):
+            os.remove('lolsig.png')
+        lol_input = (str(message.content[len(cmd_league) + 1 + len(pfx):]))
         region_x, ignore, summoner_name_x = lol_input.partition(' ')
         summoner_name = summoner_name_x.lower()
         region = region_x.lower()
-        summoner_by_name = ('https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.4/summoner/by-name/' + summoner_name + '?api_key=' + riot_api_key)
-        print(summoner_by_name)
-        summoner_by_name_json_src = urllib.request.urlopen(summoner_by_name).read().decode('utf-8')
-        summoner_by_name_json = json.loads(summoner_by_name_json_src)
+        lol_sig_url = ('http://lolsigs.com/' + summoner_name + '_' + region + '_266_0.png')
+        wget.download(lol_sig_url)
+        os.rename(summoner_name + '_' + region + '_266_0.png', 'lolsig.png')
         try:
-            smn_name = str(summoner_by_name_json[summoner_name]['name'])
-            smn_id = str(summoner_by_name_json[summoner_name]['id'])
-            smn_icon = str(summoner_by_name_json[summoner_name]['profileIconId'])
-            smn_level = str(summoner_by_name_json[summoner_name]['summonerLevel'])
-
-            stats_normal_url = ('https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.3/stats/by-summoner/' + smn_id + '/summary?season=SEASON2016&api_key=' + riot_api_key)
-            print(stats_normal_url)
-            stats_normal_json = urllib.request.urlopen(stats_normal_url).read().decode('utf-8')
-            stats_normal = json.loads(stats_normal_json)
-            stats_ranked_url = ('https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.3/stats/by-summoner/' + smn_id + '/ranked?season=SEASON2016&api_key=' + riot_api_key)
-            print(stats_ranked_url)
-            stats_ranked_json = urllib.request.urlopen(stats_ranked_url).read().decode('utf-8')
-            stats_ranked = json.loads(stats_normal_json)
-
-            norm_totalAssists = str(stats_ranked['playerStatSummaries'][0]['aggregatedStats']['totalAssists'])
-            norm_totalNeutralMinionsKilled = str(stats_ranked['playerStatSummaries']['aggregatedStats']['totalNeutralMinionsKilled'])
-            norm_totalMinionKills = str(stats_ranked['playerStatSummaries']['aggregatedStats']['totalMinionKills'])
-            norm_totalChampionKills = str(stats_ranked['playerStatSummaries']['aggregatedStats']['totalChampionKills'])
-            norm_totalTurretsKilled = str(stats_ranked['playerStatSummaries']['aggregatedStats']['averageAssists'])
-            await client.send_message(message.channel, '**Summoner Name:** ' + smn_name +
-                                    '\n**Summoner ID:** ' + smn_id +
-                                    '\n**Summoner Level:** ' + smn_level +
-                                    '\\n**Ranked:**' +
-                                    '\n   - **Kills:** ' + norm_totalChampionKills +
-                                      '\n   - **Assists:** ' + norm_totalAssists +
-                                      '\n   - **Minion Kills:** ' + norm_totalMinionKills +
-                                      '\n   - **Neutral Minion Kills:** ' + norm_totalNeutralMinionsKilled +
-                                      '\n   - **Turret Kills:** ' + norm_totalTurretsKilled)
+            await client.send_file(message.channel, 'lolsig.png')
             print('CMD [' + cmd_name + '] > ' + initiator_data)
         except KeyError:
-            try:
-                print('CMD [' + cmd_name + '] > ' + initiator_data)
-                print(summoner_by_name_json['error'])
-                await client.send_message(message.channel, 'Error:' + summoner_by_name_json['status']['status_code'] + ' - ' + summoner_by_name_json['status']['message'] + '.')
-            except:
-                print('CMD [' + cmd_name + '] > ' + initiator_data)
-                await client.send_message(message.channel, 'Something went wrong.\nThe servers are most likely overloaded, please try again.')
+            print('CMD [' + cmd_name + '] > ' + initiator_data)
+            await client.send_message(message.channel, 'Something went wrong.\nThe servers are most likely overloaded, please try again.')
     elif message.content.startswith('-read<@92747043885314048>'):
         if message.author.id == ownr:
             await client.send_message(message.channel, 'Alex stop wasting time and get back to working on my APIs...')
@@ -216,4 +183,119 @@ async def on_message(message):
             await client.send_message(message.channel, 'Ace, I heard Alex bought this giant black dildo, wanna go test it out?')
         else:
             await client.send_message(message.channel, 'No! ಠ_ಠ')
+# Blade and Soul API
+    elif message.content.startswith(pfx + cmd_bns + ' '):
+        cmd_name = 'Blade and Soul'
+        bns_input = (str(message.content[len(cmd_bns) + 1 + len(pfx):]))
+        region_x, ignore, char_name_x = bns_input.partition(' ')
+        char_name = char_name_x.lower()
+        region = region_x.lower()
+        profile_url = ('http://' + region + '-bns.ncsoft.com/ingame/bs/character/profile?c=' + char_name)
+        try:
+            profile_url = ('http://' + region + '-bns.ncsoft.com/ingame/bs/character/profile?c=' + char_name)
+            def page_raw_data():
+                page_data = urllib.request.urlopen(profile_url).read()
+                return str(page_data)
+            page_data_html = lxml.html.document_fromstring(page_raw_data())
+            page_data_extr = page_data_html.xpath("//div[@class='stat-point]'")
+            await client.send_message(message.channel, 'Retrieving data...')
+            await client.send_typing(message.channel)
+            print(page_data_extr)
+            await client.send_message(message.channel, page_data_extr)
+            print('CMD [' + cmd_name + '] > ' + initiator_data)
+        except:
+            client.send_message(message.channel, 'Something went wrong...')
+            print('CMD [' + cmd_name + '] > ' + initiator_data)
+#Urban Dictionary API
+    elif message.content.startswith(pfx + cmd_ud + ' '):
+        await client.send_typing(message.channel)
+        cmd_name = 'Urban Dictionary'
+        ud_input = (str(message.content[len(cmd_ud) + 1 + len(pfx):]))
+        url = "https://mashape-community-urban-dictionary.p.mashape.com/define?term=" + ud_input
+        headers = {'X-Mashape-Key': mashape_key, 'Accept': 'text/plain'}
+        #r = requests.get(url, headers=headers)
+        #r_dec = r.read.decode('utf-8')
+        response = requests.get(url, headers=headers).json()
+        result_type = str((response['result_type']))
+        if result_type == 'exact':
+            try:
+                definition = str((response['list'][0]['definition']))
+                permalink = str((response['list'][0]['permalink']))
+                #thumbs_up = str((response['list'][0]['thumbs_up']))
+                #thumbs_down = str((response['list'][0]['thumbs_down']))
+                example = str((response['list'][0]['example']))
+                await client.send_message(message.channel, 'Word: `' + ud_input + '`\n'
+                                          'Definition:\n```' + definition + '```\n' +
+                                          'Example:\n```' + example + '```\n' +
+                                          'Source: ```' + permalink + '```')
+                print('CMD [' + cmd_name + '] > ' + initiator_data)
+            except IndexError:
+                await client.send_message(message.channel, 'Something went wrong... The API dun goofed...')
+                print('CMD [' + cmd_name + '] > ' + initiator_data)
+        elif result_type == 'no_results':
+            try:
+                await client.send_message(message.channel, 'No results :cry:')
+                print('CMD [' + cmd_name + '] > ' + initiator_data)
+            except:
+                await client.send_message(message.channel, 'Something went wrong, and we don\'t know what!')
+                print('CMD [' + cmd_name + '] > ' + initiator_data)
+    elif message.content.startswith(pfx + cmd_weather + ' '):
+        await client.send_typing(message.channel)
+        cmd_name = 'Weather'
+        owm_input = (str(message.content[len(cmd_weather) + 1 + len(pfx):]))
+        city, ignore, country = owm_input.partition(' ')
+        owm_url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + country + '&appid=b49efc119530833da61588e4d87668c1'
+        owm_data = urllib.request.urlopen(owm_url).read().decode('utf-8')
+        owm_json = json.loads(owm_data)
+        kelvin = 273.16
+        try:
+            coord_lon = str(owm_json['coord']['lon'])
+            coord_lat = str(owm_json['coord']['lat'])
+            sys_country = str(owm_json['sys']['country'])
+            sys_city = str(owm_json['name'])
+            weather = str(owm_json['weather'][0]['main'])
+            temp = (str(round(owm_json['main']['temp'] - kelvin)) + '°C')
+            temp_f = (str(round((((owm_json['main']['temp'] - kelvin) * 9) / 5) + 32)) + '°F')
+            humidity = (str(owm_json['main']['humidity']) + '%')
+            pressure = (str(owm_json['main']['pressure']) + ' mb')
+            temp_min_c = (str(round(owm_json['main']['temp_min'] - kelvin)) + '°C')
+            temp_max_c = (str(round(owm_json['main']['temp_max'] - kelvin)) + '°C')
+            temp_min_f = (str(round((((owm_json['main']['temp_min'] - kelvin) * 9) / 5) + 32)) + '°F')
+            temp_max_f = (str(round((((owm_json['main']['temp_max'] - kelvin) * 9) / 5) + 32)) + '°F')
+            if weather == 'Thunderstorm':
+                icon = ':thunder_cloud_rain:'
+            elif weather == 'Drizzle':
+                icon = ':cloud:'
+            elif weather == 'Rain':
+                icon = ':cloud_rain:'
+            elif weather == 'Snow':
+                icon = ':cloud_snow:'
+            elif weather == 'Clear':
+                icon = ':sunny:'
+            elif weather == 'Clouds':
+                icon = ':white_sun_cloud:'
+            elif weather == 'Extreme':
+                icon = ':cloud_tornado:'
+            else:
+                icon = ':earth_americas:'
+            weather_message = ('Weather in `' + sys_city + ', ' + sys_country + '` ' +
+                               'Lat: `' + coord_lat + '` | Lon: `' + coord_lon + '`\n\n' +
+                               'Current State: `' + weather + '` ' + icon + '\n\n' +
+                               'Details:\n```Current: ' + temp + ' (' + temp_f + ')\n' +
+                               'High: ' + temp_max_c + ' (' + temp_max_f + ')\n' +
+                               'Low: ' + temp_min_c + ' (' + temp_min_f + ')\n' +
+                               'Humidity: ' + humidity + '\nPressure: ' + pressure + '\n```')
+            await client.send_message(message.channel, weather_message)
+            print('CMD [' + cmd_name + '] > ' + initiator_data)
+        except AttributeError:
+            await client.send_message(message.channel, 'Something went wrong, and we don\'t know what!')
+            print('CMD [' + cmd_name + '] > ' + initiator_data)
+        try:
+            owm_error_code = str(owm_json['cod'])
+            if owm_error_code == '404':
+                await client.send_message(message.channel, 'Error: Requested location not found!')
+                print('CMD [' + cmd_name + '] > ' + initiator_data)
+        except AttributeError:
+            await client.send_message(message.channel, 'Something went wrong, and we don\'t know what!')
+            print('CMD [' + cmd_name + '] > ' + initiator_data)
 client.run(token)
