a72384700f88d8f9644edc0cecb47fcf2c7ce3c4
AXAz0r <thealeksaradovic@gmail.com>
2016-08-26 04:28:33 +0200

    Added Joke

--- sigma.py
+++ sigma.py
@@ -62,7 +62,7 @@ cmd_ud = (commands['cmd_ud'])
 cmd_weather = (commands['cmd_weather'])
 cmd_hearthstone = (commands['cmd_hearthstone'])
 cmd_pokemon = (commands['cmd_pokemon'])
-
+cmd_joke = (commands['cmd_joke'])
 
 # I love spaghetti!
 
@@ -113,7 +113,8 @@ async def on_message(message):
                                   '\nUrban Dictionary: `' + pfx + cmd_ud + '`' +
                                   '\nWeather: `' + pfx + cmd_weather + '`' +
                                   '\nHearthstone: `' + pfx + cmd_hearthstone + '`' +
-                                  '\nPokemon: `' + pfx + cmd_pokemon + '`')
+                                  '\nPokemon: `' + pfx + cmd_pokemon + '`' +
+                                  '\nJoke: `' + pfx + cmd_joke + '`')
         print('CMD [' + cmd_name + '] > ' + initiator_data)
     # Overwatch API
     elif message.content.startswith(pfx + cmd_overwatch + ' '):
@@ -839,5 +840,33 @@ async def on_message(message):
             await client.send_message(message.channel, 'Message has been sent to <@' + ownr + '>')
         except pushbullet.errors.PushbulletError:
             await client.send_message(message.channel, 'There was a problem, probably rate limiting...')
+        print('CMD [' + cmd_name + '] > ' + initiator_data)
+    elif message.content.startswith(pfx + cmd_joke):
+        cmd_name = 'Joke'
+        number_list = [0, 1, 2]
+        joke_no = random.choice(number_list)
+        await client.send_typing(message.channel)
+        if joke_no == 0:
+            joke_type = 'Chuck Noris Joke'
+            joke_url = 'https://api.chucknorris.io/jokes/random'
+            joke_json = requests.get(joke_url).json()
+            joke = joke_json['value']
+        elif joke_no == 1:
+            joke_type = 'Ron Swanson Quote'
+            joke_url = 'http://ron-swanson-quotes.herokuapp.com/v2/quotes'
+            joke_json = requests.get(joke_url).json()
+            joke = joke_json[0]
+        elif joke_no == 2:
+            comic_no = random.randint(1, 1724)
+            joke_type = 'xkcd Story'
+            joke_url = 'http://xkcd.com/' + comic_no + '/info.0.json'
+            joke_json = requests.get(joke_url).json()
+            joke = ('#' + comic_no + ' - ' + joke_json['title'] + joke_json['transcript'])
+        else:
+            joke_type = 'Normal Joke'
+            joke_url = 'http://tambal.azurewebsites.net/joke/random'
+            joke_json = requests.get(joke_url).json()
+            joke = joke_json['joke']
+        await client.send_message(message.channel, 'Here, have a ' + joke_type + '!\n```' + joke + '\n```')
         print('CMD [' + cmd_name + '] > ' + initiator_data)
 client.run(token)
