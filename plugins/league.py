31d5003b084222739e716e5314a7fcd5f0c9f84b
awaken1ng <lucidsunlight@yandex.ru>
2016-08-27 17:03:04 +0700

    Catching up with LoL module changes

--- plugins/league.py
+++ plugins/league.py
@@ -1,8 +1,12 @@
 from plugin import Plugin
 from commands import *
-import random
+from config import RiotAPIKey as riot_api_key
 import os
 import wget
+import requests
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 
 class LeagueOfLegends(Plugin):
     is_global = True
@@ -12,34 +16,105 @@ class LeagueOfLegends(Plugin):
         if message.content.startswith(pfx + cmd_league + ' '):
             await self.client.send_typing(message.channel)
             cmd_name = 'League of Legends'
-            champ_no = ['266', '103', '84', '12', '32', '34', '1', '22', '136' '268', '432', '53', '63', '201', '51', '69',
-                        '31', '42', '122', '131', '36', '119', '245', '60', '28', '81', '9', '114', '105', '3', '41', '86',
-                        '150', '79', '104', '120', '74', '420', '39', '40', '59' '24', '126', '202', '222', '429', '43',
-                        '30', '38', '55', '10', '85', '121', '203', '240', '96', '7', '64' '89', '127', '236', '117', '99',
-                        '54', '90', '57', '11' '21' '82', '25', '267', '75', '111', '76', '56', '20', '2', '61', '80', '78',
-                        '133', '33', '421', '58', '107', '92', '68', '13', '113', '35', '98', '102', '27', '14', '15', '72',
-                        '37', '16', '50', '134', '223' '163', '91', '44', '17', '412', '0', '18', '48', '23', '4' '29',
-                        '77', '6', '110', '67', '45', '161', '254', '112', '8', '106', '19', '62', '101', '5' '157', '83',
-                        '154', '238', '115', '26', '143']
-            skin_no = ['0', '1', '2']
-            champ_back = random.choice(champ_no)
-            skin_back = random.choice(skin_no)
-            if os.path.isfile('lolsig.png'):
-                os.remove('lolsig.png')
-            lol_input = (str(message.content[len(cmd_league) + 1 + len(pfx):]))
-            region_x, ignore, summoner_name_x = lol_input.partition(' ')
-            summoner_name = summoner_name_x.lower()
-            region = region_x.lower()
-            lol_sig_url = (
-                'http://lolsigs.com/' + summoner_name + '_' + region + '_' + champ_back + '_' + skin_back + '.png')
-            wget.download(lol_sig_url)
-            os.rename(summoner_name + '_' + region + '_' + champ_back + '_' + skin_back + '.png', 'lolsig.png')
-            try:
-                await self.client.send_file(message.channel, 'lolsig.png')
-                if os.path.isfile('lolsig.png'):
-                    os.remove('lolsig.png')
-                #print('CMD [' + cmd_name + '] > ' + initiator_data)
-            except KeyError:
-                #print('CMD [' + cmd_name + '] > ' + initiator_data)
-                await self.client.send_message(message.channel,
-                                          'Something went wrong.\nThe servers are most likely overloaded, please try again.')
+            lol_input = str(message.content[len(pfx) + len(cmd_league) + 1:])
+            region, ignore, smnr_name = lol_input.lower().partition(' ')
+            smnr_name_table = smnr_name.replace(' ', '')
+            smrn_by_name_url = 'https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.4/summoner/by-name/' + smnr_name + '?api_key=' + riot_api_key
+            if region.lower() == 'eune' or 'euw' or 'na':
+                smnr_by_name = requests.get(smrn_by_name_url).json()
+                smnr_id = str(smnr_by_name[smnr_name_table]['id'])
+                smnr_icon = str(smnr_by_name[smnr_name_table]['profileIconId'])
+                icon_url = 'http://ddragon.leagueoflegends.com/cdn/6.17.1/img/profileicon/' + smnr_icon + '.png'
+                smnr_lvl = str(smnr_by_name[smnr_name_table]['summonerLevel'])
+                summary_url = 'https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.3/stats/by-summoner/' + smnr_id + '/summary?season=SEASON2016&api_key=' + riot_api_key
+                summary = requests.get(summary_url).json()
+                league_url = 'https://' + region + '.api.pvp.net/api/lol/' + region + '/v2.5/league/by-summoner/' + smnr_id + '?api_key=' + riot_api_key
+                try:
+                    league = requests.get(league_url).json()
+                    league_name = league[smnr_id][0]['name']
+                    league_tier = league[smnr_id][0]['tier']
+                except:
+                    league_name = 'No League'
+                    league_tier = 'No Rank'
+                # Image Start
+                if os.path.isfile('cache/lol/avatar_' + message.author.id + '.png'):
+                    os.remove('cache/lol/avatar_' + message.author.id + '.png')
+                if os.path.isfile('cache/lol/profile_' + message.author.id + '.png'):
+                    os.remove('cache/lol/profile_' + message.author.id + '.png')
+                avaloc = 'cache/lol/avatar_' + message.author.id + '.png'
+                wget.download(icon_url, out=avaloc)
+                # avatar_link_base = 'https://blzgdapipro-a.akamaihd.net/game/unlocks/'
+                # avatar_name = str(profile_json['data']['avatar'])
+                # os.rename(avatar_name[len(avatar_link_base):], '/cache/ow/avatar_' + message.author.id + '.png')
+                # border_link_base = 'https://blzgdapipro-a.akamaihd.net/game/playerlevelrewards/'
+                # border_name = str(profile_json['data']['levelFrame'])
+                # os.rename(border_name[len(border_link_base):], '/cache/ow/border_' + message.author.id + '.png')
+                base = Image.open('base.png')
+                overlay = Image.open('overlay_lol.png')
+                background = Image.open('cache/lol/avatar_' + message.author.id + '.png')
+                background_res = background.resize((72, 72), Image.ANTIALIAS)
+                foreground = Image.open('border_lol.png')
+                foreground_res = foreground.resize((64, 64), Image.ANTIALIAS)
+                base.paste(background_res, (28, 28))
+                base.paste(overlay, (0, 0), overlay)
+                base.paste(foreground_res, (32, 32), foreground_res)
+                font = ImageFont.truetype("big_noodle_titling_oblique.ttf", 32)
+                font2 = ImageFont.truetype("big_noodle_titling_oblique.ttf", 16)
+                font3 = ImageFont.truetype("big_noodle_titling_oblique.ttf", 48)
+                imgdraw = ImageDraw.Draw(base)
+                imgdraw.text((130, 38), smnr_name, (255, 255, 255), font=font)
+                imgdraw.text((130, 70), league_name + ' - ' + league_tier, (255, 255, 255), font=font2)
+                imgdraw.text((326, 38), smnr_lvl, (255, 255, 255), font=font3)
+                base.save('cache\lol\profile_' + message.author.id + '.png')
+                # Image End
+
+                try:
+                    item = next((item for item in summary['playerStatSummaries'] if item['playerStatSummaryType'] == 'RankedSolo5x5'), None)
+                    if item:
+                        ranked = item
+                        ranked_wins = str(ranked['wins'])
+                        ranked_losses = str(ranked['losses'])
+                        ranked_kills = str(ranked['aggregatedStats']['totalChampionKills'])
+                        ranked_minions = str(ranked['aggregatedStats']['totalMinionKills'])
+                        ranked_turrets = str(ranked['aggregatedStats']['totalTurretsKilled'])
+                        ranked_neutrals = str(ranked['aggregatedStats']['totalNeutralMinionsKilled'])
+                        ranked_assists = str(ranked['aggregatedStats']['totalAssists'])
+                        ranked_text = ('Wins: ' + ranked_wins +
+                                       '\nLosses: ' + ranked_losses +
+                                       '\nKills: ' + ranked_kills +
+                                       '\nAssists: ' + ranked_assists +
+                                       '\nMinion Kills: ' + ranked_minions +
+                                       '\nTurret Kills: ' + ranked_turrets +
+                                       '\nJungle Minion Kills: ' + ranked_neutrals)
+                    else:
+                        ranked_text = 'None'
+                except:
+                    ranked_text = 'None'
+                try:
+                    item = next((item for item in summary['playerStatSummaries'] if item['playerStatSummaryType'] == 'Unranked'), None)
+                    if item:
+                        normal = item
+                        normal_wins = str(normal['wins'])
+                        normal_kills = str(normal['aggregatedStats']['totalChampionKills'])
+                        normal_minions = str(normal['aggregatedStats']['totalMinionKills'])
+                        normal_turrets = str(normal['aggregatedStats']['totalTurretsKilled'])
+                        normal_neutrals = str(normal['aggregatedStats']['totalNeutralMinionsKilled'])
+                        normal_assists = str(normal['aggregatedStats']['totalAssists'])
+                        normal_text = ('Wins: ' + normal_wins +
+                                       '\nKills: ' + normal_kills +
+                                       '\nAssists: ' + normal_assists +
+                                       '\nMinion Kills: ' + normal_minions +
+                                       '\nTurret Kills: ' + normal_turrets +
+                                       '\nJungle Minion Kills: ' + normal_neutrals)
+                    else:
+                        normal_text = 'None'
+                except SyntaxError:
+                    normal_text = 'None'
+                if ranked_text == 'None' and normal_text == 'None':
+                    await self.client.send_message(message.channel, 'No stats found.')
+                else:
+                    await self.client.send_file(message.channel, 'cache/lol/profile_' + message.author.id + '.png')
+                    await self.client.send_message(message.channel,'Normal Stats:\n```' + normal_text + '\n```\nRanked Stats:\n```' + ranked_text + '\n```')
+            else:
+                await self.client.send_message(message.channel, 'Invalid Region: `' + region + '`.')
+            #print('CMD [' + cmd_name + '] > ' + initiator_data)
