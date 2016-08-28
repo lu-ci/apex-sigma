b6b82f832e245ca510fa5ba5daea7b4b14f747de
AXAz0r <thealeksaradovic@gmail.com>
2016-08-28 12:18:42 +0200

    LastFM Fix and Dynamic Number

--- plugins/lastfm.py
+++ plugins/lastfm.py
@@ -20,19 +20,21 @@ class LastFM(Plugin):
             lfm_url = 'http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=' + lfm_user + '&api_key=' + lfm_key + '&format=json'
             lfm_data = requests.get(lfm_url).json()
             no_of_songs_in_list = len(lfm_data['toptracks']['track'])
+            if no_of_songs == '':
+                no_of_songs = 5
             try:
                 top_tracks_text = ('Top 5 Tracks for the user `' + lfm_user + '`:\n```')
-                for i in range(0, 5):
+                for i in range(0, int(no_of_songs)):
                     name = lfm_data['toptracks']['track'][i]['name']
                     artist = lfm_data['toptracks']['track'][i]['artist']['name']
-                    top_tracks_text += '\n #' + i + ': ' + name + ' by ' + artist
+                    top_tracks_text += '\n #' + str(i+1) + ': ' + name + ' by ' + artist
                 top_tracks_text += '\n```'
                 await self.client.send_message(message.channel, top_tracks_text)
             except:
                 try:
                     await self.client.send_message(message.channel, lfm_data['message'])
                 except:
-                    #try:
-                        #await self.client.send_message(message.channel,'There are only: `' + str(no_of_songs_in_list) + '` in your list.')
-                    #except:
+                    if no_of_songs_in_list < int(no_of_songs):
+                        await self.client.send_message(message.channel,'There are only: `' + str(no_of_songs_in_list) + '` in your list.')
+                    else:
                         await self.client.send_message(message.channel, 'We seem to have ran into an error.')
