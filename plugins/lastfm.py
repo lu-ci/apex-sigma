4a81583c3d639bfb2a7139f605b69971fc83f588
awaken1ng <lucidsunlight@yandex.ru>
2016-08-28 17:08:58 +0700

    LastFM code improvement

--- plugins/lastfm.py
+++ plugins/lastfm.py
@@ -21,23 +21,12 @@ class LastFM(Plugin):
             lfm_data = requests.get(lfm_url).json()
             no_of_songs_in_list = len(lfm_data['toptracks']['track'])
             try:
-                tr1_n = lfm_data['toptracks']['track'][0]['name']
-                tr1_a = lfm_data['toptracks']['track'][0]['artist']['name']
-                tr2_n = lfm_data['toptracks']['track'][1]['name']
-                tr2_a = lfm_data['toptracks']['track'][1]['artist']['name']
-                tr3_n = lfm_data['toptracks']['track'][2]['name']
-                tr3_a = lfm_data['toptracks']['track'][2]['artist']['name']
-                tr4_n = lfm_data['toptracks']['track'][3]['name']
-                tr4_a = lfm_data['toptracks']['track'][3]['artist']['name']
-                tr5_n = lfm_data['toptracks']['track'][4]['name']
-                tr5_a = lfm_data['toptracks']['track'][4]['artist']['name']
-                top_tracks_text = ('Top 5 Tracks for the user `' + lfm_user + '`:\n```' +
-                                   '\n #1: ' + tr1_n + ' by ' + tr1_a +
-                                   '\n #2: ' + tr2_n + ' by ' + tr2_a +
-                                   '\n #3: ' + tr3_n + ' by ' + tr3_a +
-                                   '\n #4: ' + tr4_n + ' by ' + tr4_a +
-                                   '\n #5: ' + tr5_n + ' by ' + tr5_a +
-                                   '\n```')
+                top_tracks_text = ('Top 5 Tracks for the user `' + lfm_user + '`:\n```')
+                for i in range(0, 5):
+                    name = lfm_data['toptracks']['track'][i]['name']
+                    artist = lfm_data['toptracks']['track'][i]['artist']['name']
+                    top_tracks_text += '\n #' + i + ': ' + name + ' by ' + artist
+                top_tracks_text += '\n```'
                 await self.client.send_message(message.channel, top_tracks_text)
             except:
                 try:
