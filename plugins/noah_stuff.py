52eb611aa2471d3f8754554fe050d31a316e99fc
AXAz0r <thealeksaradovic@gmail.com>
2016-08-29 02:09:00 +0200

    more ifs for the noah thing

--- plugins/noah_stuff.py
+++ plugins/noah_stuff.py
@@ -15,5 +15,9 @@ class Jacob_Noah(Plugin):
                           message.author.id, message.server.name, message.server.id, message.channel)
             if message.author.id == OwnerID:
                 await self.client.send_message(message.channel, 'Of course <@' + OwnerID + '>, my love, anything for you! Chu~')
+            elif message.author.id == '113106144444030976':
+                await self.client.send_message(message.channel, 'You don\'t need me <@' + message.author.id + '>, you have a lovely Canadian girl waiting for that large rod of yours~')
+            elif message.author.id == '152239976338161664':
+                await self.client.send_message(message.channel, 'Nuuuu~... <@' + message.author.id + '>, go ask Bubu!')
             else:
                 await self.client.send_message(message.channel, 'Ew <@' + OwnerID + '>... Would kindly piss off...')
