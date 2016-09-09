4e6ffa1ef53bb817959970a6086dd5f4b49ea543
Lucid Sunlight <lucidsunlight@yandex.ru>
2016-09-10 04:44:59 +0700

    WK can now pull data by username

--- plugins/nihongo.py
+++ plugins/nihongo.py
@@ -7,7 +7,8 @@ import sqlite3
 from utils import bold
 import datetime
 import requests
-
+import json
+from lxml import html
 
 class WK(Plugin):
     is_global = True
@@ -18,80 +19,119 @@ class WK(Plugin):
             await self.client.send_typing(message.channel)
             cmd_name = 'WaniKani'
             dbsql = sqlite3.connect('storage/server_settings.sqlite', timeout=20)
-            try:
-                self.log.info('User %s [%s] on server %s [%s], used the ' + cmd_name + ' command on #%s channel',
+            try: self.log.info('User %s [%s] on server %s [%s], used the ' + cmd_name + ' command on #%s channel',
                               message.author,
                               message.author.id, message.server.name, message.server.id, message.channel)
-            except:
-                self.log.info('User %s [%s], used the ' + cmd_name + ' command.',
+            except: self.log.info('User %s [%s], used the ' + cmd_name + ' command.',
                               message.author,
                               message.author.id)
-            try:
-                for user in message.mentions:
-                    user_id = str(user.id)
-            except:
-                user_id = str(message.author.id)
-            try:
-                key_cur = dbsql.execute("SELECT WK_KEY from WANIKANI where USER_ID=" + str(user_id) + ";")
-            except:
-                key_cur = dbsql.execute("SELECT WK_KEY from WANIKANI where USER_ID=" + str(message.author.id) + ";")
-            for row in key_cur:
-                key = row[0]
-            try:
-                url = 'https://www.wanikani.com/api/user/' + key + '/srs-distribution'
-            except UnboundLocalError:
-                await self.client.send_message(message.channel,
+
+            if len(message.mentions) == 0:  #no mentions in the message
+                if message.content == (pfx + cmd_wk): user_id = str(message.author.id)  #if no arguments passed, pulling the ID of a caller
+                else:
+                    key = None
+                    username = message.content[len(pfx) + len(cmd_wk) + 1:]  #otherwise, pull the username out of a message
+            else: #if there are mentions in the message
+                try: user_id = message.mentions[0].id  #pull the mentioned ID
+                except:
+                    await self.client.send_message(message.channel, 'Error while parsing the input message')
+
+            if 'username' not in locals():
+                if 'user_id' not in locals():
+                    await self.client.send_message(message.channel, 'No arguments passed')
+                    return
+                else: #a username was passed
+                    key_cur = dbsql.execute("SELECT WK_KEY, WK_USERNAME from WANIKANI where USER_ID=" + str(user_id) + ";")
+                    db_response = key_cur.fetchone()
+                    if db_response == None:
+                        await self.client.send_message(message.channel, 'No assigned key or username was found\n'
+                                                                        'You can add it by sending me a direct message, for example\n'
+                                                                        '`' + pfx + cmd_wk_store + ' key <your API key>` or `' + pfx + cmd_wk_store + ' username <your username>`')
+                        return
+                    print(db_response)
+                    key = db_response[0]
+                    username = db_response[1]
+                    if (key == ''): key = None  #None the key if its empty so the if below won't be confused
+
+
+            #pull data to parse from
+            if key != None:  #if we have a key, pull data directly from API
+                try:
+                    url = 'https://www.wanikani.com/api/user/' + key
+                    api = requests.get(url + '/srs-distribution').json()
+                    api2 = requests.get(url + '/level-progression').json()
+                    api3 = requests.get(url + '/study-queue').json()
+                    rad = 'Radicals: Total: ' + bold(
+                        str(api2['requested_information']['radicals_total'])) + ' | Done: ' + bold(
+                        str(api2['requested_information']['radicals_progress']))
+                    kanji = 'Kanji: Total: ' + bold(str(api2['requested_information']['kanji_total'])) + ' | Done: ' + bold(
+                        str(api2['requested_information']['kanji_progress']))
+
+                    try: next_review_date = bold(datetime.datetime.fromtimestamp(api3['requested_information']['next_review_date']).strftime('%B %d, %Y %H:%M'))
+                    except TypeError: pass  #NoneType on retrival, user is on vacation
+                    lesson_queue = bold(str(api3['requested_information']['lessons_available']))
+                    review_queue = bold(str(api3['requested_information']['reviews_available']))
+                    review_nh = bold(str(api3['requested_information']['reviews_available_next_hour']))
+                    review_nd = bold(str(api3['requested_information']['reviews_available_next_day']))
+                    if api3['requested_information']['reviews_available'] > 150: warning = ':exclamation:'
+                    else: warning = ''
+                except UnboundLocalError:
+                    await self.client.send_message(message.channel,
                                                'There doesn\'t seem to be a key tied to you...\n\nYou can add your key by sending a direct message to me with the the WKSave Command, for example:\n`' + pfx + cmd_wk_store + ' 16813135183151381`\nAnd just replace the numbers with your WK API Key!')
+                    return
+                except TypeError:
+                    self.log.info('Type error')
+                    return
+            elif username != None:  #otherwise if we have a username, pull data from profile page
+                page = requests.get('https://www.wanikani.com/community/people/' + username)
+                tree = html.fromstring(page.content)
+                try: script = tree.xpath('//div[@class="footer-adjustment"]/script')[0].text.strip()
+                except IndexError:
+                    await self.client.send_message(message.channel, "Error while parsing the page, profile not found or doesn't exist")
+                    return
+                script = script[script.find('var srsCounts') : script.find('Counts.fillInSrsCount(srsCounts.requested_information);')]
+                script = script.strip()[16:-1]
+                api = json.loads(script)
+            else:
+                await self.client.send_message(message.channel, 'No key or username')
                 return
-            api = requests.get(url).json()
-            api2 = requests.get('https://www.wanikani.com/api/user/' + key + '/level-progression').json()
-            api3 = requests.get('https://www.wanikani.com/api/user/' + key + '/study-queue').json()
+
+            #parsing
             try:
+                out = ''
+
                 username = api['user_information']['username']
                 title = api['user_information']['title']
                 level = str(api['user_information']['level'])
-                creation_date = datetime.datetime.fromtimestamp(api['user_information']['creation_date']).strftime(
-                    '%B %d, %Y')
+                creation_date = datetime.datetime.fromtimestamp(api['user_information']['creation_date']).strftime('%B %d, %Y')
                 topics_count = str(api['user_information']['topics_count'])
                 posts_count = str(api['user_information']['posts_count'])
-
                 apprentice = 'Apprentice: ' + bold(str(api['requested_information']['apprentice']['total']))
                 guru = 'Guru: ' + bold(str(api['requested_information']['guru']['total']))
                 master = 'Master: ' + bold(str(api['requested_information']['master']['total']))
                 enlightned = 'Enlightened: ' + bold(str(api['requested_information']['enlighten']['total']))
                 burned = 'Burned: ' + bold(str(api['requested_information']['burned']['total']))
 
-                rad = 'Radicals: Total: ' + bold(
-                    str(api2['requested_information']['radicals_total'])) + ' | Done: ' + bold(
-                    str(api2['requested_information']['radicals_progress']))
-                kanji = 'Kanji: Total: ' + bold(str(api2['requested_information']['kanji_total'])) + ' | Done: ' + bold(
-                    str(api2['requested_information']['kanji_progress']))
 
-                next_review_date = bold(
-                    datetime.datetime.fromtimestamp(api3['requested_information']['next_review_date']).strftime(
-                        '%B %d, %Y %H:%M'))
-                lesson_queue = bold(str(api3['requested_information']['lessons_available']))
-                review_queue = bold(str(api3['requested_information']['reviews_available']))
-                review_nh = bold(str(api3['requested_information']['reviews_available_next_hour']))
-                review_nd = bold(str(api3['requested_information']['reviews_available_next_day']))
-                if api3['requested_information']['reviews_available'] > 150:
-                    warning = ':exclamation:'
-                else:
-                    warning = ''
-                out = ''
                 out += bold(username) + ' of ' + bold('Sect ' + title) + '\n'
                 out += bold('Level ' + level) + ' Apprentice' + '\n'
                 out += 'Scribed ' + bold(topics_count + ' topics') + ' & ' + bold(posts_count + ' posts') + '\n'
                 out += 'Serving the Crabigator since ' + bold(creation_date) + '\n'
                 out += apprentice + ' | ' + guru + ' | ' + master + ' | ' + enlightned + ' | ' + burned + '\n'
-                out += rad + ' ' + kanji + '\n'
-                out += 'Your Next Review: ' + next_review_date + '\n'
-                out += 'Lesson Queue: ' + lesson_queue + ' | Review Queue: ' + review_queue + warning + '\n'
-                out += 'Reviews Next Hour: ' + review_nh + ' | Reviews Next Day: ' + review_nd
-                await self.client.send_message(message.channel, out)
-            except SyntaxError:
-                await self.client.send_message(message.channel, 'Something went wrong ¯\_(ツ)_/¯')
 
+                if 'api2' in locals():
+                    out += rad + ' ' + kanji + '\n'
+
+                if 'api3' in locals():
+                    try: out += 'Your Next Review: ' + next_review_date + '\n'
+                    except UnboundLocalError: pass #no review date, user is on vacation
+                    out += 'Lesson Queue: ' + lesson_queue + ' | Review Queue: ' + review_queue + warning + '\n'
+                    out += 'Reviews Next Hour: ' + review_nh + ' | Reviews Next Day: ' + review_nd
+                await self.client.send_message(message.channel, out)
+            except:
+                self.log.info('Error while parsing the data')
+                await self.client.send_message(message.channel, 'Something went wrong ¯\_(ツ)_/¯. Error while parsing the data')
+                return
 
 class WKKey(Plugin):
     is_global = True
@@ -102,33 +142,58 @@ class WKKey(Plugin):
             await self.client.send_typing(message.channel)
             cmd_name = 'WaniKani Key Save'
             user_id = str(message.author.id)
-            wk_key = str(message.content[len(pfx) + len(cmd_wk_store) + 1:])
-            if len(wk_key) < 32 or len(wk_key) > 32:
-                await self.client.send_message(message.channel, 'The Key Seems Invalid...')
-            else:
-                sql_cmd = ("INSERT INTO WANIKANI (USER_ID, WK_KEY) \
-                                      VALUES (userid, 'wkkey')").replace('userid', user_id).replace('wkkey', wk_key)
+            self.log.info('User %s [%s], used the ' + cmd_name + ' command.', message.author, user_id)
+
+            try:
+                args = message.content[len(pfx) + len(cmd_wk_store) + 1:]
+
+                mode = args[:args.find(' ')].strip()
+                payload = args[args.find(' ') + 1:].strip() #key or username
+
+                print(args)
+                print(mode)
+                print(payload)
+
+                if mode == '':
+                    await self.client.send_message(message.channel, 'Bind your Discord profile and your API key or username\n'
+                                                                    'Usage: `'+pfx + cmd_wk_store+' key <your api key here>` or `'+pfx + cmd_wk_store+' username <your username here>`')
+                    return
+                if mode not in ['key', 'username', 'remov']:  #remove
+                    await self.client.send_message(message.channel, 'Unknown argument')
+                    return
+                if mode == 'key':
+                    if len(payload) < 32 or len(payload) > 32:
+                        await self.client.send_message(message.channel, 'The Key Seems Invalid...')
+                        return
+
                 dbsql = sqlite3.connect('storage/server_settings.sqlite', timeout=20)
-                self.log.info('User %s [%s], used the ' + cmd_name + ' command.',
-                              message.author,
-                              message.author.id)
+
+                if mode == 'remov':  #remove
+                    query = "DELETE from WANIKANI where USER_ID=" + user_id + ";"
+                    dbsql.execute(query)
+                    dbsql.commit()
+                    await self.client.send_message(message.channel, 'Record deleted')
+                    return
+
                 try:
-                    dbsql.execute(sql_cmd)
+                    if mode == 'key': query = "INSERT INTO WANIKANI (USER_ID, WK_KEY) VALUES ("+user_id+",'"+payload+"')"
+                    if mode == 'username': query = "INSERT INTO WANIKANI (USER_ID, WK_USERNAME) VALUES ("+user_id+",'"+payload+"')"
+                    dbsql.execute(query)
                     dbsql.commit()
-                    await self.client.send_message(message.channel, 'Key Safely Stored. :key:')
+                    await self.client.send_message(message.channel, mode.capitalize() + ' Safely Stored. :key:')
                 except sqlite3.IntegrityError:
-                    await self.client.send_message(message.channel, 'A Key for your User ID already exists...')
+                    await self.client.send_message(message.channel, 'A Key for your User ID already exists, removing...')
                     dbsql.execute("DELETE from WANIKANI where USER_ID=" + user_id + ";")
+                    dbsql.execute(query)
                     dbsql.commit()
-                    await self.client.send_message(message.channel, 'Old Key Removed...')
-                    dbsql.execute(sql_cmd)
-                    dbsql.commit()
-                    await self.client.send_message(message.channel, 'New Key Safely Stored. :key:')
+                    await self.client.send_message(message.channel, 'New '+mode.capitalize()+' Safely Stored. :key:')
                 except UnboundLocalError:
                     await self.client.send_message(message.channel,
-                                                   'There doesn\'t seem to be a key tied to you...\nYou can add your key by sending a direct message to me withe the WKSave Command, for example:\n`' + pfx + cmd_wk_store + ' 16813135183151381`\nand just replace the numbers with your WK API Key!')
+                                                   'There doesn\'t seem to be a key or username tied to you...\nYou can add your it by sending a direct message to me with the WKSave Command, for example:\n`' + pfx + cmd_wk_store + ' 16813135183151381`\nand just replace the numbers with your WK API Key!')
                 except SyntaxError:
                     await self.client.send_message(message.channel, 'Something went horribly wrong!')
+
+            except: await self.client.send_message(message.channel, 'Error while parsing the input message')
 
 
 class Jisho(Plugin):
