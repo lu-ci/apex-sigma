3f282685b2bf8e0e53458a9e2e022eb9266f1095
AXAz0r <thealeksaradovic@gmail.com>
2016-09-11 21:54:20 +0200

    Karaoke file made

---
+++ plugins/karaoke.py
@@ -0,0 +1,6 @@
+from plugin import Plugin
+from config import cmd_handup
+from config import cmd_handdown
+from config import cmd_takemic
+from config import cmd_dropmic
+from config import cmd_repertoire
