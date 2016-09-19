4313120de1891b79e72dc7c79f7767b46a392ca7
AXAz0r <thealeksaradovic@gmail.com>
2016-09-20 00:30:50 +0200

    Timer is now AWESOME

--- plugins/utils.py
+++ plugins/utils.py
@@ -3,6 +3,7 @@ from config import cmd_remind, donators, OwnerID as ownr, permitted_id
 import asyncio
 from utils import create_logger
 from utils import bold
+import time
 
 
 class Reminder(Plugin):
@@ -18,23 +19,30 @@ class Reminder(Plugin):
                           message.author.id, message.server.name, message.server.id, message.channel)
             remind_input = message.content[len(pfx) + len(cmd_remind) + 1:]
             try:
-                time, ignore, remind_text = str(remind_input).partition(' ')
+                time_q, ignore, remind_text = str(remind_input).partition(' ')
             except:
                 remind_text = 'Nothing'
-                time = '0'
+                time_q = '0'
                 await self.client.send_message(message.channel,
                                                'Input missing parameters.\nThe command format is **' + pfx + cmd_remind + '[time in seconds] [message]**\nExample: ' + pfx + cmd_remind + ' 60 Leeroy jenkins!')
             try:
+                time_conv = time.strftime('%H:%M:%S', time.gmtime(int(time_q)))
                 if remind_text == '':
                     remind_text = 'Nothing'
-                await self.client.send_message(message.channel, 'Okay! Reminder for\n[' + bold(
-                    str(remind_text)) + ']\nis set and will be activated in `' + time + '` seconds! :clock:')
-                await asyncio.sleep(int(time))
+                confirm_msg = await self.client.send_message(message.channel, 'Okay! Reminder for\n[' + bold(
+                    str(remind_text)) + ']\nis set and will be activated in `' + time_conv + '`! :clock:')
+                time_num = int(time_q)
+                while time_num != 0:
+                    time_num -= 1
+                    time_conv_second = time.strftime('%H:%M:%S', time.gmtime(int(time_num)))
+                    await self.client.edit_message(confirm_msg,'Okay! Reminder for\n[' + bold(
+                    str(remind_text)) + ']\nis set and will be activated in `' + time_conv_second + '`! :clock:')
+                    await asyncio.sleep(1)
                 await self.client.send_typing(message.channel)
                 await self.client.send_message(message.channel,
                                                '<@' + message.author.id + '> Time\'s up! Let\'s do this! :clock: \n :exclamation: ' + bold(
                                                    str(remind_text)) + ' :exclamation: ')
-            except:
+            except SyntaxError:
                 await self.client.send_message(message.channel,
                                                'Something went wrong with setting the timer, are you sure you inputed a number?')
 
