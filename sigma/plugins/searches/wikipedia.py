334d496a36c213a30cbba5e3ddbdd04083736a3b
Aleksa Radovic <thealeksaradovic@gmail.com>
2016-10-19 06:50:17 +0200

    Update wikipedia.py

--- sigma/plugins/searches/wikipedia.py
+++ sigma/plugins/searches/wikipedia.py
@@ -21,4 +21,12 @@ class Wikipedia(Plugin):
                               message.author.id)
             q = str(message.content[len(pfx) + len('wiki') + 1:]).lower()
             result = wikipedia.summary(q)
-            await self.reply(result)
+            if result is not None:
+                out_text = 'Your search for `' + q + '` results:\n```'
+                out_text += '\n' + result
+                out_text += '\n```'
+                if len(out_text) >= 650:
+                    out_text = out_text[:650] + '...\n```'
+            else:
+                out_text = 'Nothing could be found...'
+            await self.reply(out_text)
