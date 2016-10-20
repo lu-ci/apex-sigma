f5d780f6f0691c720f5cf800e8481be9361202ef
Patrick Auernig <patrick.auernig@mykolab.com>
2016-10-20 16:05:11 +0200

    update searches/wikipedia plugin

---
+++ sigma/plugins/searches/wikipedia/wiki.py
@@ -0,0 +1,20 @@
+import wikipedia
+
+from sigma.core.formatting import code, codeblock
+
+
+async def wiki(cmd, message, args):
+    q = ' '.join(args).lower()
+
+    result = wikipedia.summary(q)
+
+    if result is not None:
+        if len(result) >= 650:
+            result = result[:650] + ' ...'
+
+        out_text = 'Your search results for {:s}:\n{:s}'.format(
+                code(q), codeblock(result))
+    else:
+        out_text = 'Nothing could be found...'
+
+    await cmd.reply(out_text)
