3b99f6ade6495b051cba58448a5d6720ba4b9c8e
AXAz0r <thealeksaradovic@gmail.com>
2016-10-20 22:05:53 +0200

    Steam Fix

--- sigma/plugins/games/steam/steam.py
+++ sigma/plugins/games/steam/steam.py
@@ -8,29 +8,44 @@ async def steam(cmd, message, args):
     try:
         steamapi = steamwebapi(SteamAPI)
         steam_input = ' '.join(args)
-
         userID = steamapi.ISteamUser.ResolveVanityURL(vanityurl=steam_input, url_type=1)['response']['steamid']
         summary = steamapi.ISteamUser.GetPlayerSummaries(steamids=userID)['response']['players'][0]
-
-        displayName = summary['personaname']
+        displayName = str(summary['personaname'])
         currentStamp = int(round(time.time()))
-        lastOnline = currentStamp - int(summary['lastlogoff'])
-        lastOnline = time.strftime('%H:%M:%S', time.gmtime(lastOnline))
         creation = currentStamp - int(summary['timecreated'])
-        creation = creation / 60 / 60 / 24 / 365.25
-
+        lastOnline = currentStamp - int(summary['lastlogoff'])
+        creation = int(creation) / 60 / 60 / 24 / 365.25
+        lastOnline = time.strftime('%H:%M:%S', time.gmtime(int(lastOnline)))
+        onlineNow = summary['personastate']
+        if (onlineNow):
+            try:
+                currentGame = summary['gameextrainfo']
+            except:
+                currentGame = 'None'
         avatar = str(summary['avatarfull'])
+        gameCount = \
+            steamapi.IPlayerService.GetOwnedGames(steamid=userID, include_appinfo=False, include_played_free_games=True,
+                                                  appids_filter=-1)['response']['game_count']
+        gameCountNonFree = steamapi.IPlayerService.GetOwnedGames(steamid=userID, include_appinfo=False,
+                                                                 include_played_free_games=False, appids_filter=-1)[
+            'response']['game_count']
+        if (onlineNow):
+            await cmd.reply(str('Display name : ' + displayName +
+                                '\nTime on steam: ' + str(creation)[
+                                                      -1:] + ' years' +
+                                '\nThis User is Currently Online' +
+                                '\navatar: ' + avatar +
+                                '\nnumber of games: ' + str(
+                gameCount) + ', of which ' + str(gameCount - gameCountNonFree) + ' are free.'))
+        else:
+            await cmd.reply(str('Display name : ' + displayName +
+                                '\nTime on steam: ' + str(creation)[
+                                                      -1:] + ' years' +
+                                '\nlast Online: ' + str(lastOnline) + ' ago' +
+                                '\navatar: ' + avatar +
+                                '\nnumber of games: ' + str(
+                gameCount) + ', of which ' + str(gameCount - gameCountNonFree) + ' are free.'))
 
-        gameCount = steamapi.IPlayerService.GetOwnedGames(steamid=userID, include_appinfo=False, include_played_free_games=True, appids_filter=-1)['response']['game_count']
-        gameCountNonFree = steamapi.IPlayerService.GetOwnedGames(steamid=userID, include_appinfo=False, include_played_free_games=False, appids_filter=-1)['response']['game_count']
-
-        msg = 'Display name: {:s}\n'
-        msg += 'Time on steam: {:s} years\n'
-        msg += 'last Online: {:s} ago\n'
-        msg += 'avatar: {:s}\n'
-        msg += 'number of games: {:d}, of wich {:d} are free.'
-        await cmd.reply(msg.format(displayName, str(creation)[-1:], lastOnline,
-                        avatar, gameCount, gameCount - gameCountNonFree))
-    except Exception as e:
-        cmd.log.error(e)
-        await cmd.reply('an unknown error occured. is that your vanity URL?')
+    except SyntaxError as e:
+        await cmd.reply('An unknown error ocoured. is that your vanity URL?')
+        print(e)
