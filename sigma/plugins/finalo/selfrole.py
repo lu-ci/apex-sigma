2b87600381a7015ef1abb443885121e9d066a591
AXAz0r <thealeksaradovic@gmail.com>
2016-10-20 21:20:19 +0200

    Selfrole Translate

--- sigma/plugins/finalo/selfrole.py
+++ sigma/plugins/finalo/selfrole.py
@@ -19,23 +19,23 @@ cmd_addAssingableRole = 'addar'
 cmd_removeAssingableRole = 'remar'
 
 
-async def on_message(self, message, pfx):
+async def on_message(ev, message, pfx):
     if message.channel.id == '222882496113672193':
         if message.content == 'Pokemon':
             message.content = 'Pokémon'
-        if message.author.id == self.client.user.id:
+        if message.author.id == ev.user.id:
             return
         else:
-            await self.client.delete_message(message)
+            await ev.bot.delete_message(message)
         if message.content.startswith(pfx + 'dumproles'):
             if message.author.id in permitted_id:
                 out = '['
                 for role in self_roles:
                     out += "'" + role + "', "
                 out = out[:-2] + ']'
-                outmsg = await self.client.send_message(message.channel, out)
+                outmsg = await ev.reply(message.channel, out)
                 asyncio.sleep(timeout)
-                await self.client.delete_message(outmsg)
+                await ev.bot.delete_message(outmsg)
             else:
                 'Insufficient permissions'
 
@@ -43,17 +43,17 @@ async def on_message(self, message, pfx):
             role_list = 'Currently assingable roles: \n'
             for role in self_roles:
                 role_list += '`' + role + '`, '
-            outmsg = await self.client.send_message(message.channel, role_list[:-2])
+            outmsg = await ev.reply(message.channel, role_list[:-2])
             asyncio.sleep(5)
-            await self.client.delete_message(outmsg)
+            await ev.bot.delete_message(outmsg)
         if message.content.startswith(pfx + cmd_addAssingableRole):
             if message.author.id in permitted_id:
                 added_role = message.content[len(pfx) + len(cmd_addAssingableRole) + 1:]
                 if added_role in self_roles:
-                    outmsg = await self.client.send_message(message.channel,
-                                                            'Role is already in the list, aborting')
+                    outmsg = await ev.reply(message.channel,
+                                            'Role is already in the list, aborting')
                     asyncio.sleep(timeout)
-                    await self.client.delete_message(outmsg)
+                    await ev.bot.delete_message(outmsg)
                     return
 
                 server_roles = []
@@ -62,17 +62,17 @@ async def on_message(self, message, pfx):
 
                 if added_role in server_roles:
                     self_roles.append(added_role)
-                    outmsg = await self.client.send_message(message.channel,
-                                                            'Role `{}` is added to the list'.format(added_role))
+                    outmsg = await ev.reply(message.channel,
+                                            'Role `{}` is added to the list'.format(added_role))
                     asyncio.sleep(timeout)
-                    await self.client.delete_message(outmsg)
+                    await ev.bot.delete_message(outmsg)
                     return
                 else:
-                    outmsg = await self.client.send_message(message.channel,
-                                                            'Role `{}` is not found on the server, aborting'.format(
-                                                                added_role))
+                    outmsg = await ev.reply(message.channel,
+                                            'Role `{}` is not found on the server, aborting'.format(
+                                                added_role))
                     asyncio.sleep(timeout)
-                    await self.client.delete_message(outmsg)
+                    await ev.bot.delete_message(outmsg)
                     return
             else:
                 'Insufficient permissions'
@@ -83,18 +83,18 @@ async def on_message(self, message, pfx):
 
                 if removed_role in self_roles:
                     self_roles.remove(removed_role)
-                    outmsg = await self.client.send_message(message.channel,
-                                                            'Role `{}` was removed from the list'.format(
-                                                                removed_role))
+                    outmsg = await ev.reply(message.channel,
+                                            'Role `{}` was removed from the list'.format(
+                                                removed_role))
                     asyncio.sleep(timeout)
-                    await self.client.delete_message(outmsg)
+                    await ev.bot.delete_message(outmsg)
                     return
                 else:
-                    outmsg = await self.client.send_message(message.channel,
-                                                            'Role `{}` is not on the list, aborting'.format(
-                                                                removed_role))
+                    outmsg = await ev.reply(message.channel,
+                                            'Role `{}` is not on the list, aborting'.format(
+                                                removed_role))
                     asyncio.sleep(timeout)
-                    await self.client.delete_message(outmsg)
+                    await ev.bot.delete_message(outmsg)
                     return
             else:
                 'Insufficient permissions'
@@ -111,22 +111,22 @@ async def on_message(self, message, pfx):
                 for role in message.server.roles:
                     if role.name == message.content:
                         if user_has_role:
-                            await self.client.remove_roles(message.author, role)
-                            response = await self.client.send_message(message.channel,
-                                                                      '<@{0}> Role `{1}` removed'.format(
-                                                                          message.author.id, role.name))
+                            await ev.bot.remove_roles(message.author, role)
+                            response = await ev.reply(message.channel,
+                                                      '<@{0}> Role `{1}` removed'.format(
+                                                          message.author.id, role.name))
 
                             await asyncio.sleep(timeout)
-                            await self.client.delete_message(response)
+                            await ev.bot.delete_message(response)
 
                             return
                         else:
-                            await self.client.add_roles(message.author, role)
-                            response = await self.client.send_message(message.channel,
-                                                                      '<@{0}> Role `{1}` assigned'.format(
-                                                                          message.author.id, role.name))
+                            await ev.bot.add_roles(message.author, role)
+                            response = await ev.reply(message.channel,
+                                                      '<@{0}> Role `{1}` assigned'.format(
+                                                          message.author.id, role.name))
 
                             await asyncio.sleep(timeout)
-                            await self.client.delete_message(response)
+                            await ev.bot.delete_message(response)
 
                             return
