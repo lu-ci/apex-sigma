29291c8db48915a08d7684c716f663a06358892e
Patrick Auernig <patrick.auernig@mykolab.com>
2016-10-22 21:13:44 +0200

    no "call" in batch (?)

--- .ci/jenkins.bat
+++ .ci/jenkins.bat
@@ -1,6 +1,6 @@
 @echo off
 set "VIRTENV_DIR=sigma.env"
 
-call "virtualenv --python=C:\\Python35\\python.exe %VIRTENV_DIR%"
-call "%VIRTENV_DIR%\\Scripts\\activate.bat"
-call "pip install -r requirements.txt"
+virtualenv --python=C:\\Python35\\python.exe %VIRTENV_DIR%
+%VIRTENV_DIR%\\Scripts\\activate.bat
+pip install -r requirements.txt
