8999849dafb043449b857abacfda405a8db5e1a4
Patrick Auernig <patrick.auernig@mykolab.com>
2016-10-23 01:33:46 +0200

    second try with colored logging

--- sigma/core/logger.py
+++ sigma/core/logger.py
@@ -2,6 +2,7 @@ import os
 from time import time
 from datetime import datetime as date
 import logging
+import coloredlogs
 
 
 log_fmt = '%(levelname)-6s %(asctime)s %(name)-20s %(message)s'
@@ -13,11 +14,13 @@ if not os.path.exists(log_dir):
 logfile_name = date.fromtimestamp(time()).strftime('%Y%m%d-%H%M%S') + '.log'
 log_file = os.path.join(log_dir, logfile_name)
 
+cformatter = coloredlogs.ColoredFormatter(log_fmt)
 formatter = logging.Formatter(log_fmt)
+
 
 def create_logger(name):
     stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
+    stream_handler.setFormatter(cformatter)
 
     file_handler = logging.FileHandler(log_file)
     file_handler.setFormatter(formatter)
