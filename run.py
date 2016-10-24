849ecd7cbc6242cbd4d8b87eb97660057f039fef
AXAz0r <thealeksaradovic@gmail.com>
2016-10-24 04:36:57 +0200

    My Shit was REMOVED... i mean the login

--- run.py
+++ run.py
@@ -5,8 +5,6 @@ import os
 from sigma.core import Sigma
 
 from config import Token as token
-from config import StartupType, dsc_email, dsc_password
-
 
 if __name__ == '__main__':
 
@@ -18,23 +16,9 @@ if __name__ == '__main__':
 
     client = Sigma()
 
-    if StartupType == '0':
-        if token == '':
-            sys.exit('Token not provided, please open config.py and place your token.')
-
-        try:
-            client.run(token)
-        except Exception as e:
-            client.log.error(e)
-
-    elif StartupType == '1':
-        if dsc_email == '' or dsc_password == '':
-            sys.exit('Discord Email and/or Passoword not provided, please open config.py and fill in those details.')
-
-        try:
-            client.run(dsc_email, dsc_password)
-        except Exception as err:
-            client.log.error(err)
-    else:
-        client.log.error('Failed loading connection settings.\nCheck your StartupType and make sure it\'s either 0 or 1.')
-        sys.exit('Startup Type is not found.')
+    if token == '':
+        sys.exit('Token not provided, please open config.py and place your token.')
+    try:
+        client.run(token)
+    except Exception as e:
+        client.log.error(e)
