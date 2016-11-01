ff31c7898909b96790f55d8ba93a3a9c67bc68a4
AXAz0r <thealeksaradovic@gmail.com>
2016-11-01 15:01:41 +0100

    DAMN I'M AWESOME

---
+++ sigma/plugins/polaris/moderation/roles.py
@@ -0,0 +1,12 @@
+async def roles(cmd, message, args):
+    n = 0
+    role_list = ''
+    for role in message.server.roles:
+        n += 1
+        role_list += role.name + ', '
+    role_list = role_list[:-2].replace('@', '')
+    if len(role_list) > 1800:
+        role_list = role_list[:1800]
+    out_text = 'There is a total of **' + str(n) + '** roles on **' + message.server.name + '**:\n'
+    out_text += '```\n' + role_list + '\n```'
+    await cmd.reply(out_text)
