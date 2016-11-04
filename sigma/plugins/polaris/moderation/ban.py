b1f6f7d10af4ad0b224078d18b4610e379329fcb
AXAz0r <thealeksaradovic@gmail.com>
2016-11-04 22:52:05 +0100

    Ban Fixed, Unban Added

--- sigma/plugins/polaris/moderation/ban.py
+++ sigma/plugins/polaris/moderation/ban.py
@@ -10,7 +10,7 @@ async def ban(cmd, message, args):
             if check_ban(message.author, channel):
                 try:
                     await cmd.bot.ban(user_q)
-                    await cmd.reply('User **' + user_q.name + '** has been softbanned.')
+                    await cmd.reply('User **' + user_q.name + '** has been banned.')
                 except Exception as e:
                     cmd.log.error(e)
                     await cmd.reply(str(e))
@@ -18,3 +18,7 @@ async def ban(cmd, message, args):
                 response = await cmd.reply('Only a user with **Ban** privileges can use this command. :x:')
                 await asyncio.sleep(10)
                 await cmd.bot.delete_message(response)
+        else:
+            await cmd.reply(cmd.help())
+    else:
+        await cmd.reply(cmd.help())
