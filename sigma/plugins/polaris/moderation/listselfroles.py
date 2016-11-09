40d6fe6c205524c4b1f76948d09d07d50c16aac8
AXAz0r <thealeksaradovic@gmail.com>
2016-11-09 22:13:13 +0100

    ListSelf Roles for Mongo

--- sigma/plugins/polaris/moderation/listselfroles.py
+++ sigma/plugins/polaris/moderation/listselfroles.py
@@ -1,20 +1,19 @@
 async def listselfroles(cmd, message, args):
     try:
-        db_exists_check = 'SELECT EXISTS (SELECT ROLE_NAME FROM SELF_ROLE WHERE SERVER_ID=?);'
-        db_role_select_all = 'SELECT ROLE_NAME FROM SELF_ROLE WHERE SERVER_ID=?'
+        check_data = {
+            'ServerID': message.server.id
+        }
         role_list = []
         exists = 0
-        db_exists_data = cmd.db.execute(db_exists_check, message.server.id)
-        for result in db_exists_data:
-            exists = result[0]
+        check_results = cmd.db.find('SelfRoles', check_data)
+        for result in check_results:
+            exists += 1
+            if exists > 0:
+                role_list.append(result['RoleName'])
         if exists == 0:
             await cmd.bot.send_message(message.channel, 'No self assignable roles exist on this server.')
             return
         else:
-            db_role_select_data = cmd.db.execute(db_role_select_all, message.server.id)
-            for result in db_role_select_data:
-                for entry in result:
-                    role_list.append(entry.lower())
             await cmd.bot.send_message(message.channel, 'List of self assiglable roles for ' + message.server.name + ' is:\n```\n' + ', '.join(
                 role_list) + '\n```')
     except SyntaxError as e:
