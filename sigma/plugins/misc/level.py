55800c368cb6c9904aa6ad3716f4107871e398a1
AXAz0r <thealeksaradovic@gmail.com>
2016-11-10 23:07:19 +0100

    Level System Change

--- sigma/plugins/misc/level.py
+++ sigma/plugins/misc/level.py
@@ -21,7 +21,7 @@ async def level(cmd, message, args):
             pass
     modifier = points / 1690
     modifier = int(modifier)
-    level = points / (690 + (69 * modifier))
+    level = points / (169 + (69 * modifier))
     points = str(points)
     level = str(level).split('.')[0]
     msg = 'Okay, <@{:s}>{:s} **Level {:s}** and currently {:s} **{:s} Points**!'
