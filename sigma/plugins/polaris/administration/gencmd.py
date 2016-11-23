966d426a6a9af50ffada682baa7ad8716c5633bf
AXAz0r <thealeksaradovic@gmail.com>
2016-11-23 06:57:19 +0100

    Command List Categories

--- sigma/plugins/polaris/administration/gencmd.py
+++ sigma/plugins/polaris/administration/gencmd.py
@@ -11,6 +11,7 @@ async def gencmd(cmd, message, args):
         out_text += '\nCommand |  Description |  Usage'
         out_text += '\n--------|--------------|-------'
         n = 0
+        category_curr = 'None'
         for root, dirs, files in os.walk(directory):
             for file in files:
                 if file == 'plugin.yml':
@@ -18,11 +19,22 @@ async def gencmd(cmd, message, args):
                     with open(file_path) as plugin_file:
                         plugin_data = yaml.load(plugin_file)
                         try:
+                            category = plugin_data['categories'][0]
+                        except:
+                            pass
+                        try:
                             for command in plugin_data['commands']:
                                 plugin_name = command['name']
                                 plugin_usage = command['usage'].replace('{pfx:s}', Prefix).replace('{cmd:s}',
                                                                                                    plugin_name)
                                 plugin_desc = command['description']
+                                if category == category_curr:
+                                    pass
+                                else:
+                                    category_curr = category
+                                    out_text += '\n###' + category.upper()
+                                    out_text += '\nCommand |  Description |  Usage'
+                                    out_text += '\n--------|--------------|-------'
                                 out_text += '\n`>>' + plugin_name + '`  |  ' + plugin_desc + '  |  `' + plugin_usage + '`'
                                 n += 1
                         except:
