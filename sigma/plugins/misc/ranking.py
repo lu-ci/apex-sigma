b3566a2e6a3eae533083116e4e9cbf94dc3cba7c
AXAz0r <thealeksaradovic@gmail.com>
2016-11-28 06:34:11 +0100

    Force Point Check

--- sigma/plugins/misc/ranking.py
+++ sigma/plugins/misc/ranking.py
@@ -1,3 +1,3 @@
 async def ranking(cmd, message, args):
     await cmd.bot.send_message(message.channel,
-                               'You can find the ranking for **' + message.server.name + '** on <https://auroraproject.xyz/ranking?sid=' + message.server.id + '>')
+                               'You can find the ranking for **' + message.server.name + '** here:\n<https://auroraproject.xyz/ranking?sid=' + message.server.id + '>')
