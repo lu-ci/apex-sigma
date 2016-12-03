fa7d6c2f26846fcb27d750fd86b7f2c9cd85036c
AXAz0r <thealeksaradovic@gmail.com>
2016-12-04 00:02:15 +0100

    Added Cooldown Control to Database and Up to Kud

--- sigma/plugins/misc/reward.py
+++ sigma/plugins/misc/reward.py
@@ -1,48 +1,15 @@
 import random
-import time
+
 
 async def reward(ev, message, args):
     if message.server is None:
         return
     if message.author.bot:
         return
-    current_timestamp = int(time.time())
-    cooldown_finder_data = {
-        'Type': 'Activity',
-        'UserID': message.author.id,
-        'ServerID': message.server.id,
-    }
-    cooldown_insert_data = {
-        'Type': 'Activity',
-        'UserID': message.author.id,
-        'ServerID': message.server.id,
-        'LastTimestamp': current_timestamp,
-    }
-    cooldown_data = ev.db.find('Cooldowns', cooldown_finder_data)
-    n = 0
-    last_use = 0
-    for result in cooldown_data:
-        n += 1
-        try:
-            last_use = result['LastTimestamp']
-        except:
-            last_use = 0
-    if n == 0:
-        off_cooldown = True
-        not_in_db = True
-    else:
-        not_in_db = False
-        if current_timestamp > last_use + 60:
-            off_cooldown = True
-        else:
-            off_cooldown = False
-    if off_cooldown:
-        if not_in_db:
-            ev.db.insert_one('Cooldowns', cooldown_insert_data)
-        else:
-            updatetarget = {"UserID": message.author.id, "ServerID": message.server.id, "Type": "Activity"}
-            updatedata = {"$set": {"LastTimestamp": current_timestamp}}
-            ev.db.update_one('Cooldowns', updatetarget, updatedata)
+    cd_state = ev.db.on_cooldown(message.server.id, message.author.id, 'Activity', 60)
+    if not cd_state:
+        ev.db.set_cooldown(message.server.id, message.author.id, 'Activity')
+        print('point')
         target = None
         n = 0
         collection = 'PointSystem'
@@ -77,4 +44,3 @@ async def reward(ev, message, args):
                 'Level': level
             }}
             ev.db.update_one(collection, updatetarget, updatedata)
-
