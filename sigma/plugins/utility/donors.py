f754a332454030451dc88afd8c00b61c5aeffa6e
AXAz0r <thealeksaradovic@gmail.com>
2016-12-13 04:52:09 +0100

    Donors Fixed

--- sigma/plugins/utility/donors.py
+++ sigma/plugins/utility/donors.py
@@ -7,16 +7,17 @@ async def donors(cmd, message, args):
         donors = content['donors']
     don_lst = []
     for don in donors:
-        temp_lst = []
-        temp_lst.append(don['name'])
+        temp_lst = [don['name']]
         don_lst.append(temp_lst)
     donor_list = format_pretty_table(don_lst)
     out_text = 'The lovely people that support the **Aurora Project**:'
     out_text += '```haskell'
     out_text += '\n' + donor_list
     out_text += '\n```'
-    out_text += '\nPlease consider donating by following instructions on the Donor page.'
-    out_text += '\n<https://auroraproject.xyz/donors>!'
+    out_text += '\nPlease consider donating by visiting the paypal.me page here:'
+    out_text += '\n<https://www.paypal.me/AleksaRadovic>'
+    out_text += '\nThe current donors can be seen here!'
+    out_text += '\n<https://auroraproject.xyz#donors>!'
     out_text += '\nThank you! :ribbon:'
 
     await cmd.bot.send_message(message.channel, out_text)
