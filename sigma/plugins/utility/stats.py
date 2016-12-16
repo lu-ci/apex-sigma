ccc8ee93ac0e02c90e3c29d03116dc4b922c3822
AXAz0r <thealeksaradovic@gmail.com>
2016-12-16 21:43:27 +0100

    Removed Stats Auto Deletion

--- sigma/plugins/utility/stats.py
+++ sigma/plugins/utility/stats.py
@@ -1,13 +1,7 @@
 from sigma.core.stats import stats as sigma_stats
 from sigma.core.formatting import codeblock
-import asyncio
 
 async def stats(cmd, message, args):
     out = sigma_stats(cmd.bot)
-
     out_txt = codeblock('\n'.join(out), syntax='haskell')
-
-    stats_msg = await cmd.bot.send_message(message.channel, out_txt)
-    await asyncio.sleep(60)
-    await cmd.bot.delete_message(message)
-    await cmd.bot.delete_message(stats_msg)
+    await cmd.bot.send_message(message.channel, out_txt)
