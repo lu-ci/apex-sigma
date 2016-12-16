d4054734d23e840bdac0493e704f6ec00efab747
AXAz0r <thealeksaradovic@gmail.com>
2016-12-16 23:35:02 +0100

    DM Check

--- sigma/plugins/moderation/bye.py
+++ sigma/plugins/moderation/bye.py
@@ -2,25 +2,26 @@ from sigma.core.permission import check_admin
 
 
 async def bye(cmd, message, args):
-    if not check_admin(message.author, message.channel):
-        await cmd.bot.send_message(message.channel, ':x: Insufficient permissions.\nServer admin only.')
-        return
-    else:
-        active = cmd.db.get_settings(message.server.id, 'Bye')
-        bye_channel = cmd.db.get_settings(message.server.id, 'ByeChannel')
-        if not bye_channel:
-            bye_channel = message.server.default_channel.id
-        if not active:
-            cmd.db.set_settings(message.server.id, 'Bye', True)
-            cmd.db.set_settings(message.server.id, 'ByeChannel', message.channel.id)
-            await cmd.bot.send_message(message.channel,
-                                       ':white_check_mark: Notifying of leaving members has been **enabled** on <#' + message.channel.id + '>.')
+    if message.server:
+        if not check_admin(message.author, message.channel):
+            await cmd.bot.send_message(message.channel, ':x: Insufficient permissions.\nServer admin only.')
+            return
         else:
-            if message.channel.id == bye_channel:
-                cmd.db.set_settings(message.server.id, 'Bye', False)
-                await cmd.bot.send_message(message.channel,
-                                           ':fire: Notifying of leaving members has been **disabled**.')
-            else:
+            active = cmd.db.get_settings(message.server.id, 'Bye')
+            bye_channel = cmd.db.get_settings(message.server.id, 'ByeChannel')
+            if not bye_channel:
+                bye_channel = message.server.default_channel.id
+            if not active:
+                cmd.db.set_settings(message.server.id, 'Bye', True)
                 cmd.db.set_settings(message.server.id, 'ByeChannel', message.channel.id)
                 await cmd.bot.send_message(message.channel,
-                                           ':white_check_mark: The leaving members message channel has been changed to <#' + message.channel.id + '>.')
+                                           ':white_check_mark: Notifying of leaving members has been **enabled** on <#' + message.channel.id + '>.')
+            else:
+                if message.channel.id == bye_channel:
+                    cmd.db.set_settings(message.server.id, 'Bye', False)
+                    await cmd.bot.send_message(message.channel,
+                                               ':fire: Notifying of leaving members has been **disabled**.')
+                else:
+                    cmd.db.set_settings(message.server.id, 'ByeChannel', message.channel.id)
+                    await cmd.bot.send_message(message.channel,
+                                               ':white_check_mark: The leaving members message channel has been changed to <#' + message.channel.id + '>.')
--- sigma/plugins/moderation/greet.py
+++ sigma/plugins/moderation/greet.py
@@ -2,24 +2,25 @@ from sigma.core.permission import check_admin
 
 
 async def greet(cmd, message, args):
-    if not check_admin(message.author, message.channel):
-        await cmd.bot.send_message(message.channel, ':x: Insufficient permissions.\nServer admin only.')
-        return
-    else:
-        active = cmd.db.get_settings(message.server.id, 'Greet')
-        greet_channel = cmd.db.get_settings(message.server.id, 'GreetChannel')
-        if not greet_channel:
-            greet_channel = message.server.default_channel.id
-        if not active:
-            cmd.db.set_settings(message.server.id, 'Greet', True)
-            cmd.db.set_settings(message.server.id, 'GreetChannel', message.channel.id)
-            await cmd.bot.send_message(message.channel,
-                                       ':white_check_mark: Greeting new members has been **enabled** on <#' + message.channel.id + '>.')
+    if message.server:
+        if not check_admin(message.author, message.channel):
+            await cmd.bot.send_message(message.channel, ':x: Insufficient permissions.\nServer admin only.')
+            return
         else:
-            if message.channel.id == greet_channel:
-                cmd.db.set_settings(message.server.id, 'Greet', False)
-                await cmd.bot.send_message(message.channel, ':fire: Greeting new members has been **disabled**.')
-            else:
+            active = cmd.db.get_settings(message.server.id, 'Greet')
+            greet_channel = cmd.db.get_settings(message.server.id, 'GreetChannel')
+            if not greet_channel:
+                greet_channel = message.server.default_channel.id
+            if not active:
+                cmd.db.set_settings(message.server.id, 'Greet', True)
                 cmd.db.set_settings(message.server.id, 'GreetChannel', message.channel.id)
                 await cmd.bot.send_message(message.channel,
-                                           ':white_check_mark: The greeting message channel has been changed to <#' + message.channel.id + '>.')
+                                           ':white_check_mark: Greeting new members has been **enabled** on <#' + message.channel.id + '>.')
+            else:
+                if message.channel.id == greet_channel:
+                    cmd.db.set_settings(message.server.id, 'Greet', False)
+                    await cmd.bot.send_message(message.channel, ':fire: Greeting new members has been **disabled**.')
+                else:
+                    cmd.db.set_settings(message.server.id, 'GreetChannel', message.channel.id)
+                    await cmd.bot.send_message(message.channel,
+                                               ':white_check_mark: The greeting message channel has been changed to <#' + message.channel.id + '>.')
