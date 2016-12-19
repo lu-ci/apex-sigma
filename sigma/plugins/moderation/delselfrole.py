46b299279f0f9302b3cae2fcbbb7740d554c3fde
AXAz0r <thealeksaradovic@gmail.com>
2016-12-19 20:57:36 +0100

    SelfRole Fix

--- sigma/plugins/moderation/delselfrole.py
+++ sigma/plugins/moderation/delselfrole.py
@@ -31,7 +31,6 @@ async def delselfrole(cmd, message, args):
             cmd.db.set_settings(message.server.id, 'SelfRoles', self_roles)
             out_content = discord.Embed(type='rich', color=0x66cc66,
                                         title=':white_check_mark: Role **' + target_role.name + '** removed from the self assignable roles list.')
-            await cmd.bot.create_role(message.server, name=role_qry)
             await cmd.bot.send_message(message.channel, None, embed=out_content)
         else:
             out_content = discord.Embed(type='rich', color=0xFF9900, title=':warning: Error')
