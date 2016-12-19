90a2319711da468cd6444b14a437f1a55ab4eeec
AXAz0r <thealeksaradovic@gmail.com>
2016-12-19 06:58:46 +0100

    SearchID Embedding

--- sigma/plugins/administration/searchid.py
+++ sigma/plugins/administration/searchid.py
@@ -1,4 +1,5 @@
 from config import permitted_id
+import discord
 
 
 async def searchid(cmd, message, args):
@@ -8,10 +9,17 @@ async def searchid(cmd, message, args):
             return
         else:
             search_id = args[0]
-        out = '```haskell'
+        count = 0
+        embed = discord.Embed(title=':information_source: User Found On The Following Servers', color=0x0099FF)
         for server in cmd.bot.servers:
             for user in server.members:
                 if user.id == search_id:
-                    out += '\nUser ' + user.name + '#' + user.discriminator + ' [' + user.id + ']' + ' has been found on ' + server.name + ' [' + server.id + '].'
-        out += '\n```'
-        await cmd.bot.send_message(message.channel, out)
+                    count += 1
+                    embed.add_field(name=server.name, value=server.id)
+        if count == 0:
+            embed = discord.Embed(title=':information_source: User Not Found', color=0x0099FF)
+        await cmd.bot.send_message(message.channel, None, embed=embed)
+    else:
+        out = discord.Embed(type='rich', color=0xDB0000,
+                            title=':no_entry: Insufficient Permissions. Bot Owner Only.')
+        await cmd.bot.send_message(message.channel, None, embed=out)
