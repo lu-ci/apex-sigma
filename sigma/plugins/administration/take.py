c9786d587c5e80d55ef55f11a71f17fdad8f19e2
AXAz0r <thealeksaradovic@gmail.com>
2016-12-19 05:58:18 +0100

    Take embedding

--- sigma/plugins/administration/take.py
+++ sigma/plugins/administration/take.py
@@ -1,44 +1,39 @@
 from config import permitted_id
 from sigma.core.permission import check_admin
+import discord
 
 
 async def take(cmd, message, args):
-    if message.author.id in permitted_id or check_admin(message.author, message.channel):
-        if message.mentions:
-            target = message.mentions[0]
-            if target.bot:
-                await cmd.bot.send_message(message.channel, 'Can\'t award bots.')
-                return
-            qry = {
-                'ServerID': message.server.id,
-                'UserID': target.id
-            }
-            data = cmd.db.find('PointSystem', qry)
-            curr_pts = 0
-            for result in data:
-                curr_pts = result['Points']
-            try:
-                amount = abs(int(args[0]))
-                new_pts = curr_pts - amount
-            except:
-                await cmd.bot.send_message(message.channel, 'Not a valid point number input.')
-                return
-            if new_pts < 0:
-                await cmd.bot.send_message(message.channel, 'That user does not even have that many points.')
-                return
-            updatetarget = {"UserID": target.id, "ServerID": message.server.id}
-            updatedata = {"$set": {
-                'Points': new_pts
-            }}
-            cmd.db.update_one('PointSystem', updatetarget, updatedata)
-            await cmd.bot.send_message(message.channel, 'Okay <@' + message.author.id + '>. I have taken away **' + str(
-                amount) + '** points from <@' + target.id + '>!')
-            try:
-                await cmd.bot.send_message(target, 'Sorry...! :fire:\n**' + str(
-                    amount) + '** points have been taken away from you on **' + message.server.name + '** by **' + message.author.name + '**.')
-            except:
-                pass
+    if message.server:
+        if message.author.id in permitted_id or check_admin(message.author, message.channel):
+            if message.mentions:
+                target = message.mentions[0]
+                if target.bot:
+                    out = discord.Embed(title=':exclamation: Can\'t award bots.', color=0xDB0000)
+                    await cmd.bot.send_message(message.channel, None, embed=out)
+                    return
+                try:
+                    amount = abs(int(args[0]))
+                except:
+                    out = discord.Embed(title=':exclamation: Invalid Input.', color=0xDB0000)
+                    await cmd.bot.send_message(message.channel, None, embed=out)
+                    return
+                cmd.db.take_points(message.server, target, amount)
+                out = discord.Embed(title=':white_check_mark: Done', color=0x66CC66)
+                out.add_field(name='Taken From', value=target.name + '#' + target.discriminator)
+                out.add_field(name='Amount', value=str(amount))
+                await cmd.bot.send_message(message.channel, None, embed=out)
+                try:
+                    out = discord.Embed(title=':fire: Points Were Taken Away', color=0xFF9900)
+                    out.add_field(name='Server', value=message.server.name)
+                    out.add_field(name='By', value=message.author.name + '#' + message.author.discriminator)
+                    out.add_field(name='Amount', value=str(amount))
+                    await cmd.bot.send_message(target, None, embed=out)
+                except:
+                    pass
+            else:
+                await cmd.bot.send_message(message.channel, cmd.help())
         else:
-            await cmd.bot.send_message(message.channel, cmd.help())
-    else:
-        await cmd.bot.send_message(message.channel, 'Insufficient Permissions.')
+            status = discord.Embed(type='rich', color=0xDB0000,
+                                   title=':no_entry: Insufficient Permissions. Bot Owner or Server Admin Only.')
+            await cmd.bot.send_message(message.channel, None, embed=status)
