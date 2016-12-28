e516ea4bb1a4b90fa54976af8c4b425d1a6575a4
AXAz0r <thealeksaradovic@gmail.com>
2016-12-28 07:02:48 +0100

    ServerInfo Embed

--- sigma/plugins/utility/serverinfo.py
+++ sigma/plugins/utility/serverinfo.py
@@ -1,4 +1,6 @@
-from humanfriendly.tables import format_pretty_table as boop
+import discord
+
+
 async def serverinfo(cmd, message, args):
     out_list = []
     if message.server:
@@ -7,13 +9,13 @@ async def serverinfo(cmd, message, args):
         serv = message.server
         for user in serv.members:
             if user.bot:
-                bot_count +=1
+                bot_count += 1
             else:
-                user_count +=1
+                user_count += 1
         out_list.append(['Name', serv.name])
         out_list.append(['Server ID', serv.id])
         out_list.append(['Created', serv.created_at])
-        out_list.append(['Default Channel', serv.default_channel])
+        out_list.append(['Default Channel', '#' + serv.default_channel.name])
         out_list.append(['Member Count', str(user_count) + ' (+' + str(bot_count) + ' bots)'])
         out_list.append(['Owner', serv.owner])
         out_list.append(['Owner ID', serv.owner_id])
@@ -23,5 +25,8 @@ async def serverinfo(cmd, message, args):
         if serv.afk_channel:
             out_list.append(['AFK Channel', serv.afk_channel])
             out_list.append(['AFK Timeout', serv.afk_timeout])
-        out_text = '```\n' + boop(out_list) + '\n```'
-        await cmd.bot.send_message(message.channel, out_text)
+        embed = discord.Embed(title=serv.name + ' Information', color=0x1ABC9C)
+        for item in out_list:
+            embed.add_field(name=str(item[0]), value='```python\n' + str(item[1]) + '\n```')
+
+        await cmd.bot.send_message(message.channel, None, embed=embed)
