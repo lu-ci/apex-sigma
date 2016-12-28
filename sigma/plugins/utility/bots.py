4cae54d0477913dbc2b0de74aee325250f6f1f47
AXAz0r <thealeksaradovic@gmail.com>
2016-12-28 05:29:26 +0100

    Bots Embed

--- sigma/plugins/utility/bots.py
+++ sigma/plugins/utility/bots.py
@@ -1,17 +1,25 @@
-from humanfriendly.tables import format_pretty_table as boop
+import discord
 
 
 async def bots(cmd, message, args):
-    bot_list = []
-    cols = ['Name', 'Status']
+    online_bots = []
+    offline_bots = []
+    total_bots = 0
     for user in message.server.members:
         if user.bot:
+            total_bots += 1
             name = user.name + '#' + user.discriminator
-            status = str(user.status).replace('dnd', 'do not disturb').title()
-            bot_list.append([name, status])
-    out = '```haskell\n' + boop(bot_list, cols) + '\n```'
-    if len(bot_list) == 0:
-        await cmd.bot.send_message(message.channel, 'No bots were found on this server.')
-        return
+            status = str(user.status)
+            if status == 'offline':
+                offline_bots.append(name)
+            else:
+                online_bots.append(name)
+
+    if total_bots == 0:
+        embed = discord.Embed(title=':exclamation: No bots were found on this server.', color=0xDB0000)
+        await cmd.bot.send_message(message.channel, None, embed=embed)
     else:
-        await cmd.bot.send_message(message.channel, out)
+        embed = discord.Embed(title='Bot Status on ' + message.server.name, color=0x1ABC9C)
+        embed.add_field(name='Online', value='```\n - ' + '\n - '.join(sorted(online_bots)) + '\n```')
+        embed.add_field(name='Offline', value='```\n' + ' - ' + '\n - '.join(sorted(offline_bots)) + '\n```')
+    await cmd.bot.send_message(message.channel, None, embed=embed)
