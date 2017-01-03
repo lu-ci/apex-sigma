da3539141d0972b76350f7ba843c7a6484e552f5
AXAz0r <thealeksaradovic@gmail.com>
2017-01-03 19:35:08 +0100

    Osu Dynamic Color With User Color

--- sigma/plugins/games/osu/osu.py
+++ sigma/plugins/games/osu/osu.py
@@ -2,13 +2,9 @@ import discord
 
 
 async def osu(cmd, message, args):
-    try:
-        osu_input = ' '.join(args)
-
-        sig_url = 'https://lemmmy.pw/osusig/sig.php?colour=pink&uname=' + osu_input
-        embed = discord.Embed(title=osu_input.upper(), color=0xff0066)
-        embed.set_image(url=sig_url)
-        await cmd.bot.send_message(message.channel, None, embed=embed)
-    except Exception as e:
-        cmd.log.error(e)
-        await cmd.bot.send_message(message.channel, 'Something went wrong or the user was not found.')
+    osu_input = ' '.join(args)
+    user_color = str(message.author.color)[1:]
+    sig_url = 'https://lemmmy.pw/osusig/sig.php?colour=hex' + user_color + '&uname=' + osu_input
+    embed = discord.Embed(color=message.author.color)
+    embed.set_image(url=sig_url)
+    await cmd.bot.send_message(message.channel, None, embed=embed)
