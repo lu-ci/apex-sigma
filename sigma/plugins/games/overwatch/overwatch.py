423429bdaf8ddd29f778ae1644e452c583aa5df9
AXAz0r <thealeksaradovic@gmail.com>
2017-01-03 11:54:40 +0100

    Overwatch Refactor For Simple Cache

--- sigma/plugins/games/overwatch/overwatch.py
+++ sigma/plugins/games/overwatch/overwatch.py
@@ -1,7 +1,6 @@
 import os
-import wget
-import json
-import urllib.request
+from io import BytesIO
+import requests
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
@@ -14,53 +13,35 @@ async def overwatch(cmd, message, args):
     ow_region_x, ignore, ow_name = ow_input.partition(' ')
     ow_region = ow_region_x.lower().replace('na', 'us')
 
-    if os.path.isfile('cache/ow/avatar_' + message.author.id + '.png'):
-        os.remove('cache/ow/avatar_' + message.author.id + '.png')
-    if os.path.isfile('cache/ow/border_' + message.author.id + '.png'):
-        os.remove('cache/ow/border_' + message.author.id + '.png')
-    if os.path.isfile('cache/ow/profile_' + message.author.id + '.png'):
-        os.remove('cache/ow/profile_' + message.author.id + '.png')
-    if os.path.isfile('cache/ow/rank_' + message.author.id + '.png'):
-        os.remove('cache/ow/rank_' + message.author.id + '.png')
     if ow_region.upper() == 'NA' or 'US' or 'EU':
         try:
             profile = (
                 'http://api.lootbox.eu/pc/' + ow_region.lower() + '/' + ow_name + '/profile').replace(' ', '')
-            profile_json_source = urllib.request.urlopen(profile).read().decode('utf-8')
-            profile_json = json.loads(profile_json_source)
+            profile_json = requests.get(profile).json()
             champ_get_url = (
                 'http://api.lootbox.eu/pc/' + ow_region.lower() + '/' + ow_name + '/quickplay/heroes').replace(' ', '')
-            champ_get_src = urllib.request.urlopen(champ_get_url).read().decode('utf-8')
-            champ_get = json.loads(champ_get_src)
+            champ_get = requests.get(champ_get_url).json()
             avatar_link = profile_json['data']['avatar']
             border_link = profile_json['data']['levelFrame']
             if str(champ_get[0]['name']) == 'L&#xFA;cio':
                 top_champ = 'Lucio'
             else:
                 top_champ = str(champ_get[0]['name'])
-            avaloc = 'cache/ow/avatar_' + message.author.id + '.png'
-            borloc = 'cache/ow/border_' + message.author.id + '.png'
-            wget.download(avatar_link, out=avaloc)
-            # avatar_link_base = 'https://blzgdapipro-a.akamaihd.net/game/unlocks/'
-            # avatar_name = str(profile_json['data']['avatar'])
-            # os.rename(avatar_name[len(avatar_link_base):], '/cache/ow/avatar_' + message.author.id + '.png')
-            wget.download(border_link, out=borloc)
-            # border_link_base = 'https://blzgdapipro-a.akamaihd.net/game/playerlevelrewards/'
-            # border_name = str(profile_json['data']['levelFrame'])
-            # os.rename(border_name[len(border_link_base):], '/cache/ow/border_' + message.author.id + '.png')
+            avatar = requests.get(avatar_link).content
+            border = requests.get(border_link).content
             base = Image.open(cmd.resource('img/base.png'))
             overlay = Image.open(cmd.resource('img/overlay.png'))
-            foreground = Image.open('cache/ow/border_' + message.author.id + '.png')
+            foreground = Image.open(BytesIO(border))
             foreground_res = foreground.resize((128, 128), Image.ANTIALIAS)
-            background = Image.open('cache/ow/avatar_' + message.author.id + '.png')
+            background = Image.open(BytesIO(avatar))
             background_res = background.resize((72, 72), Image.ANTIALIAS)
             try:
                 rank_link = profile_json['data']['competitive']['rank_img']
-                rankloc = 'cache/ow/rank_' + message.author.id + '.png'
-                wget.download(rank_link, out=rankloc)
-                rankimg = Image.open('cache/ow/rank_' + message.author.id + '.png')
+                rank = requests.get(rank_link).content
+                rankimg = Image.open(BytesIO(rank))
                 rankimg_res = rankimg.resize((64, 64), Image.ANTIALIAS)
             except:
+                rankimg_res = None
                 pass
             base.paste(background_res, (28, 28))
             base.paste(overlay, (0, 0), overlay)
@@ -74,7 +55,7 @@ async def overwatch(cmd, message, args):
             imgdraw = ImageDraw.Draw(base)
             imgdraw.text((130, 38), profile_json['data']['username'], (255, 255, 255), font=font)
             imgdraw.text((130, 70), 'Most Played: ' + top_champ, (255, 255, 255), font=font2)
-            base.save('cache\ow\profile_' + message.author.id + '.png')
+            base.save('cache/ow_profile_' + message.author.id + '.png')
             name = profile_json['data']['username']
             level = str(profile_json['data']['level'])
             qg_won = str(profile_json['data']['games']['quick']['wins'])
@@ -112,7 +93,8 @@ async def overwatch(cmd, message, args):
                                  '\n    - Quick: ' + qg_playtime +
                                  '\n    - Competitive: ' + cg_playtime + '```')
             # print('CMD [' + cmd_name + '] > ' + initiator_data)
-            await cmd.bot.send_file(message.channel, 'cache\ow\profile_' + message.author.id + '.png')
+            await cmd.bot.send_file(message.channel, 'cache/ow_profile_' + message.author.id + '.png')
+            os.remove('cache/ow_profile_' + message.author.id + '.png')
             await cmd.bot.send_message(message.channel, overwatch_profile)
         except KeyError:
             try:
