69fe86489345fac972a33b4e0ca8ab18d5462a4f
AXAz0r <thealeksaradovic@gmail.com>
2017-01-04 09:03:02 +0100

    Link To Profile Added To Osu Command

--- sigma/plugins/games/osu/osu.py
+++ sigma/plugins/games/osu/osu.py
@@ -5,6 +5,6 @@ async def osu(cmd, message, args):
     osu_input = ' '.join(args)
     user_color = str(message.author.color)[1:]
     sig_url = 'https://lemmmy.pw/osusig/sig.php?colour=hex' + user_color + '&uname=' + osu_input
-    embed = discord.Embed(color=message.author.color)
+    embed = discord.Embed(color=message.author.color, title=osu_input.upper() + '\'s Osu! Profile', url='https://osu.ppy.sh/u/' + osu_input)
     embed.set_image(url=sig_url)
     await cmd.bot.send_message(message.channel, None, embed=embed)
