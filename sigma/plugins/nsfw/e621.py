713e1441b1cd4e936bad2fcd1ab9cfbbd645c5ff
AXAz0r <thealeksaradovic@gmail.com>
2017-01-04 12:33:47 +0100

    Added E621

---
+++ sigma/plugins/nsfw/e621.py
@@ -0,0 +1,19 @@
+import discord
+import random
+from lxml import html
+import requests
+
+
+async def e621(cmd, message, args):
+    url_base = 'http://e621.net/post/index.xml'
+    if args:
+        url = url_base + '?tags=' + '+'.join(args).replace(' ', '_')
+    else:
+        url = url_base + '?tags=nude'
+    data = requests.get(url).content
+    posts = html.fromstring(data)
+    post = random.choice(posts)
+    image_url = post.find('file_url').text
+    embed = discord.Embed(color=0x9933FF)
+    embed.set_image(url=image_url)
+    await cmd.bot.send_message(message.channel, None, embed=embed)
