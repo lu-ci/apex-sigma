a62fdea3386687d730db2ca378250b44ae8b780b
AXAz0r <thealeksaradovic@gmail.com>
2017-01-08 23:47:55 +0100

    Black Jack Command
    No ace detection yet so ace is worth only 1

---
+++ sigma/plugins/minigames/black_jack_backend.py
@@ -0,0 +1,38 @@
+blackjack_instances = {}
+symbols = ['♥', '♠', '♣', '♦']
+suits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+
+
+def get_bj(inid):
+    global blackjack_instances
+    if inid in blackjack_instances:
+        return blackjack_instances[inid]
+    else:
+        return None
+
+
+def add_bj(inid, instance_data):
+    global blackjack_instances
+    if inid not in blackjack_instances:
+        blackjack_instances.update(instance_data)
+    else:
+        print('Instance Exists')
+        raise KeyError
+
+
+def del_bj(inid):
+    global blackjack_instances
+    if inid in blackjack_instances:
+        del blackjack_instances[inid]
+    else:
+        print('Instance Non Existent')
+        raise KeyError
+
+
+def upd_bj(inid, new_data):
+    global blackjack_instances
+    if inid in blackjack_instances:
+        blackjack_instances.update(new_data)
+    else:
+        print('Instance Non Existent')
+        raise KeyError
