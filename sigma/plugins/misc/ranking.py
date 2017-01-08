e91fac59a26857bcf1782cdd00ec381dcdcd970c
AXAz0r <thealeksaradovic@gmail.com>
2017-01-08 18:10:06 +0100

    Ranking URL Change

--- sigma/plugins/misc/ranking.py
+++ sigma/plugins/misc/ranking.py
@@ -1,8 +1,9 @@
 import discord
+from config import MainServerURL
 
 
 async def ranking(cmd, message, args):
     embed = discord.Embed(color=0x1ABC9C)
     embed.add_field(name='Sigma Ranking for ' + message.server.name,
-                    value='You can click [HERE](https://auroraproject.xyz/ranking?sid=' + message.server.id + ') to see the Sigma Ranking for this server.')
+                    value='You can click [HERE](' + MainServerURL + 'ranking?sid=' + message.server.id + ') to see the Sigma Ranking for this server.')
     await cmd.bot.send_message(message.channel, None, embed=embed)
