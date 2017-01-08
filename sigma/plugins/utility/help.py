712890798375300bce407dd14127066287400d4b
AXAz0r <thealeksaradovic@gmail.com>
2017-01-08 18:12:22 +0100

    Help URL Change

--- sigma/plugins/utility/help.py
+++ sigma/plugins/utility/help.py
@@ -1,21 +1,21 @@
 import discord
-from config import Prefix
+from config import Prefix, MainServerURL
 
 
 async def help(cmd, message, args):
     if not args:
         help_out = discord.Embed(type='rich', title=':grey_question: Help', color=0x1B6F5F)
-        help_out.set_author(name='Apex Sigma', url='https://auroraproject.xyz/',
+        help_out.set_author(name='Apex Sigma', url=MainServerURL,
                             icon_url='https://i.imgur.com/s0aVvn7.png')
-        help_out.add_field(name='Website', value='[**LINK**](https://auroraproject.xyz/)')
-        help_out.add_field(name='Commands', value='[**LINK**](https://auroraproject.xyz/commands)')
+        help_out.add_field(name='Website', value='[**LINK**](' + MainServerURL + ')')
+        help_out.add_field(name='Commands', value='[**LINK**](' + MainServerURL + 'commands)')
         help_out.add_field(name='GitHub', value='[**LINK**](https://github.com/aurora-pro/apex-sigma)')
         help_out.add_field(name='AP Discord', value='[**LINK**](https://discordapp.com/invite/Ze9EfTd)')
         help_out.add_field(name='Add Me To Discord',
                            value='[**LINK**](https://discordapp.com/oauth2/authorize?client_id=' + cmd.bot.user.id + '&scope=bot&permissions=8)')
         if message.server:
             help_out.add_field(name='Ranking For This Server',
-                               value='[**LINK**](https://auroraproject.xyz/ranking?sid=' + message.server.id + ')')
+                               value='[**LINK**](' + MainServerURL + 'ranking?sid=' + message.server.id + ')')
         help_out.set_footer(
             text='For additional info and help on how to use a command use [' + Prefix + 'help COMMAND_NAME] (Example: ' + Prefix + 'help slots).')
         await cmd.bot.send_message(message.channel, None, embed=help_out)
