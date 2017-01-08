313cb0680f07ac84da3d132a3a59842950d22124
AXAz0r <thealeksaradovic@gmail.com>
2017-01-09 00:05:25 +0100

    Quitting Refunds Half

--- sigma/plugins/minigames/bjquit.py
+++ sigma/plugins/minigames/bjquit.py
@@ -6,10 +6,10 @@ async def bjquit(cmd, message, args):
     instance_id = message.server.id + message.author.id
     instance = get_bj(instance_id)
     if instance:
-        cmd.db.add_points(message.server, message.author, instance['Bet'])
+        cmd.db.add_points(message.server, message.author, instance['Bet'] // 2)
         del_bj(instance_id)
         embed = discord.Embed(color=0xFF9900,
-                              title=':fire: Too bad... Your ' + str(instance['Bet']) + ' have been refunded.')
+                              title=':fire: Too bad... Your ' + str(instance['Bet'] // 2) + ' have been refunded.')
     else:
         embed = discord.Embed(color=0xDB0000, title=':exclamation: No active blackjack games found for you.')
     await cmd.bot.send_message(message.channel, None, embed=embed)
