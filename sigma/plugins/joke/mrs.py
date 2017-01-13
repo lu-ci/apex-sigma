debec2c42ce0256819f4bfa82a67a0779a47c808
AXAz0r <thealeksaradovic@gmail.com>
2017-01-13 17:03:33 +0100

    MRS Command

---
+++ sigma/plugins/joke/mrs.py
@@ -0,0 +1,10 @@
+import asyncio
+
+
+async def mrs(cmd, message, args):
+    voice = await cmd.bot.join_voice_channel(message.author.voice_channel)
+    player = voice.create_ffmpeg_player(cmd.resource('mrs.wav'))
+    player.start()
+    while not player.is_done():
+        asyncio.sleep(1)
+    await voice.disconnect()
