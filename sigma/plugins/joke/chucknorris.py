2e4307469fccad7b9e6d7f55dbb8272adae8f42d
AXAz0r <thealeksaradovic@gmail.com>
2017-01-16 21:15:21 +0100

    Added Icons for Dadjoke and Chuck Norris

--- sigma/plugins/joke/chucknorris.py
+++ sigma/plugins/joke/chucknorris.py
@@ -8,8 +8,8 @@ async def chucknorris(cmd, message, args):
     joke_url = 'https://api.chucknorris.io/jokes/random'
     joke_json = requests.get(joke_url).json()
     joke = joke_json['value']
-    out = '```yaml\n\"'
+    out = '```\n'
     out += joke
-    out += '\"\n```'
-    embed.add_field(name='A Chuck Norris Joke', value=out)
+    out += '\n```'
+    embed.add_field(name='💪 A Chuck Norris Joke', value=out)
     await cmd.bot.send_message(message.channel, None, embed=embed)
