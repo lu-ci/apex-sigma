bfa04146017e366995fc9ca1525fd0dabcab11b4
AXAz0r <thealeksaradovic@gmail.com>
2017-01-16 21:28:03 +0100

    Yo Momma Embed

--- sigma/plugins/joke/yomomma.py
+++ sigma/plugins/joke/yomomma.py
@@ -1,14 +1,13 @@
 import requests
+import discord
 
 
 async def yomomma(cmd, message, args):
-    try:
-        resource = 'http://api.yomomma.info/'
-        data = requests.get(resource).json()
-        joke = data['joke']
-        if not joke.endswith('.'):
-            joke += '.'
-        await cmd.bot.send_message(message.channel, joke)
-    except Exception as e:
-        cmd.log.error(e)
-        await cmd.bot.send_message(message.channel, str(e))
+    resource = 'http://api.yomomma.info/'
+    data = requests.get(resource).json()
+    joke = data['joke']
+    if not joke.endswith('.'):
+        joke += '.'
+    embed = discord.Embed(color=0x1abc9c)
+    embed.add_field(name='😂 A Yo Momma Joke', value='```\n' + joke + '\n```')
+    await cmd.bot.send_message(message.channel, None, embed=embed)
