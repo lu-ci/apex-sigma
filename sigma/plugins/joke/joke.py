eb02e1d47f0bc44fad92bf61c69550c6fb0a19bf
AXAz0r <thealeksaradovic@gmail.com>
2017-01-16 21:06:37 +0100

    Joke Embed

--- sigma/plugins/joke/joke.py
+++ sigma/plugins/joke/joke.py
@@ -1,8 +1,11 @@
 import requests
+import discord
 
 
 async def joke(cmd, message, args):
     joke_url = 'http://tambal.azurewebsites.net/joke/random'
     joke_json = requests.get(joke_url).json()
-    joke = joke_json['joke']
-    await cmd.bot.send_message(message.channel, 'Here, have this!\n```' + joke + '\n```')
+    joke_text = joke_json['joke']
+    embed = discord.Embed(color=0x1abc9c)
+    embed.add_field(name='😆 Have A Random Joke', value='\n```' + joke_text + '\n```')
+    await cmd.bot.send_message(message.channel, None, embed=embed)
