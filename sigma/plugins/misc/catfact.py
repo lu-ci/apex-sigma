2854d68804c4a11950bc39b250b598236f5f508b
AXAz0r <thealeksaradovic@gmail.com>
2017-01-21 16:25:47 +0100

    CatFact Embedding

--- sigma/plugins/misc/catfact.py
+++ sigma/plugins/misc/catfact.py
@@ -1,12 +1,11 @@
 import requests
+import discord
 
 
 async def catfact(cmd, message, args):
-    try:
         resource = 'http://catfacts-api.appspot.com/api/facts'
         data = requests.get(resource).json()
         fact = data['facts'][0]
-        await cmd.bot.send_message(message.channel, 'Did you know:\n```\n' + fact + '\n```')
-    except Exception as e:
-        cmd.log.error(e)
-        await cmd.bot.send_message(message.channel, str(e))
+        embed = discord.Embed(color=0x1abc9c)
+        embed.add_field(name=':cat: Did you know...', value='```\n' + fact + '\n```')
+        await cmd.bot.send_message(message.channel, None, embed=embed)
