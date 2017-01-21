335df88a73a7a1493d2de6f9b3f60744c23120c6
AXAz0r <thealeksaradovic@gmail.com>
2017-01-21 16:28:08 +0100

    Remove Old Stats Method From CB

--- sigma/plugins/misc/cleverbot_control.py
+++ sigma/plugins/misc/cleverbot_control.py
@@ -6,6 +6,7 @@ cb = Cleverbot('Apex-Sigma')
 async def cleverbot_control(ev, message, args):
     active = ev.db.get_settings(message.server.id, 'CleverBot')
     if active:
+        ev.db.add_stats('CBCount')
         mention = '<@' + ev.bot.user.id + '>'
         mention_alt = '<@!' + ev.bot.user.id + '>'
         if message.content.startswith(mention) or message.content.startswith(mention_alt):
@@ -19,17 +20,3 @@ async def cleverbot_control(ev, message, args):
                 return
             response = cb.ask(cb_input)
             await ev.bot.send_message(message.channel, '<@' + message.author.id + '> ' + response)
-            find_data = {
-                'Role': 'Stats'
-            }
-            find_res = ev.db.find('Stats', find_data)
-            count = 0
-            for res in find_res:
-                try:
-                    count = res['CBCount']
-                except:
-                    count = 0
-            new_count = count + 1
-            updatetarget = {"Role": 'Stats'}
-            updatedata = {"$set": {"CBCount": new_count}}
-            ev.db.update_one('Stats', updatetarget, updatedata)
