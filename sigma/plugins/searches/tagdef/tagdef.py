647880f9ebdb6a0ad6870a4f02ab8a06e00323f3
AXAz0r <thealeksaradovic@gmail.com>
2017-01-25 13:48:14 +0100

    Tagdef Embed

--- sigma/plugins/searches/tagdef/tagdef.py
+++ sigma/plugins/searches/tagdef/tagdef.py
@@ -1,19 +1,17 @@
 import requests
+import discord
 from config import MashapeKey
+
 
 async def tagdef(cmd, message, args):
     if not args:
         await cmd.bot.send_message(message.channel, cmd.help())
-    else:
-        hashtag = (' '.join(args)).replace('#', '')
-        try:
-            url = "https://tagdef.p.mashape.com/one." + hashtag + '.json'
-            headers = {'X-Mashape-Key': MashapeKey, 'Accept': 'text/plain'}
-            response = requests.get(url, headers=headers).json()
-            result = response['defs']['def']['text']
-            out_text = 'The definition for **#' + hashtag + '**:'
-            out_text += '\n```\n' + result + '\n```'
-            await cmd.bot.send_message(message.channel, out_text)
-        except Exception as e:
-            cmd.log.error(e)
-            await cmd.bot.send_message(message.channel, 'The Definition for **#' + hashtag + '** was not found.')
+        return
+    hashtag = (' '.join(args)).replace('#', '')
+    url = "https://tagdef.p.mashape.com/one." + hashtag + '.json'
+    headers = {'X-Mashape-Key': MashapeKey, 'Accept': 'text/plain'}
+    response = requests.get(url, headers=headers).json()
+    result = response['defs']['def']['text']
+    embed = discord.Embed(color=0x1abc9c)
+    embed.add_field(name='#⃣ Definition of `#' + hashtag + '`', value='```\n' + result + '\n```')
+    await cmd.bot.send_message(message.channel, None, embed=embed)
