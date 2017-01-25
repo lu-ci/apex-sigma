4a94e14b4c0f5016310a828a74a2a722b0868d39
AXAz0r <thealeksaradovic@gmail.com>
2017-01-25 13:15:50 +0100

    IMDB Embed

--- sigma/plugins/searches/imdb/imdb.py
+++ sigma/plugins/searches/imdb/imdb.py
@@ -1,46 +1,41 @@
 import requests
+import discord
 
 
 async def imdb(cmd, message, args):
+    if not args:
+        return
+
     imdb_imput = ' '.join(args)
-
     request = requests.get('http://www.omdbapi.com/?t=' + imdb_imput + '&y=&plot=short&r=json').json()
-
-    try:
-        title = request['Title']
-        rated = request['Rated']
-        released = request['Released']
-        runtime = request['Runtime']
-        genre = request['Genre']
-        awards = request['Awards']
-        score = request['Metascore']
-        rating = request['imdbRating']
-        language = request['Language']
-        country = request['Country']
-        writers = request['Writer']
-        directors = request['Director']
-        actors = request['Actors']
-        plot = request['Plot']
-        movie_text = ('```\nTitle: ' + title +
-                      '\nReleased: ' + released +
-                      '\nRated: ' + rated +
-                      '\nRuntime: ' + runtime +
-                      '\nGenre: ' + genre +
-                      '\nCountry: ' + country +
-                      '\nLanguage: ' + language +
-                      '\nAwards: ' + awards +
-                      '\nWriters: ' + writers +
-                      '\nDirectors: ' + directors +
-                      '\nActors: ' + actors +
-                      '\nMetascore: ' + score +
-                      '\nIMDB Rating: ' + rating + '```')
-        await cmd.bot.send_message(message.channel, movie_text + '\nPlot:\n```\n' + plot + '\n```')
-    except:
-        if imdb_imput == '':
-            await cmd.bot.send_message(message.channel, 'You need to specify a movie!')
-        else:
-            try:
-                await cmd.bot.send_message(message.channel, 'Error: ' + request['Error'])
-            except Exception as e:
-                cmd.log.error(e)
-                await cmd.bot.send_message(message.channel, 'Something went horribly wrong!')
+    title = request['Title']
+    rated = request['Rated']
+    released = request['Released']
+    runtime = request['Runtime']
+    genre = request['Genre']
+    awards = request['Awards']
+    score = request['Metascore']
+    rating = request['imdbRating']
+    language = request['Language']
+    country = request['Country']
+    writers = request['Writer']
+    directors = request['Director']
+    actors = request['Actors']
+    plot = request['Plot']
+    movie_text = ('```\nTitle: ' + title +
+                  '\nReleased: ' + released +
+                  '\nRated: ' + rated +
+                  '\nRuntime: ' + runtime +
+                  '\nGenre: ' + genre +
+                  '\nCountry: ' + country +
+                  '\nLanguage: ' + language +
+                  '\nAwards: ' + awards +
+                  '\nWriters: ' + writers +
+                  '\nDirectors: ' + directors +
+                  '\nActors: ' + actors +
+                  '\nMetascore: ' + score +
+                  '\nIMDB Rating: ' + rating + '```')
+    embed = discord.Embed(color=0x1abc9c)
+    embed.add_field(name='🎥 Movie Details', value=movie_text)
+    embed.add_field(name='📑 Plot', value='```\n' + plot + '\n```')
+    await cmd.bot.send_message(message.channel, None, embed=embed)
