a9e5c0abbcd66b2203b1da9c67ef0040513546f6
AXAz0r <thealeksaradovic@gmail.com>
2017-01-25 19:44:12 +0100

    Reddit NSFW Check

--- sigma/plugins/searches/reddit/reddit.py
+++ sigma/plugins/searches/reddit/reddit.py
@@ -1,18 +1,24 @@
 import praw
 import random
+import discord
 from config import RedditClientID, RedditClientSecret
+from sigma.core.permission import check_channel_nsfw
 
 
 async def reddit(cmd, message, args):
     q = ' '.join(args)
-
     req = praw.Reddit(user_agent='Apex Sigma', client_id=RedditClientID, client_secret=RedditClientSecret)
-
-    posts = req.subreddit(str(q)).hot(limit=100)
+    sub = req.subreddit(str(q))
+    if sub.over18:
+        nsfw_allowed = check_channel_nsfw(cmd.db, message.channel.id)
+        if not nsfw_allowed:
+            embed_content = discord.Embed(title=':eggplant: Channel does not have NSFW permissions set, sorry.',
+                                          color=0x9933FF)
+            await cmd.bot.send_message(message.channel, None, embed=embed_content)
+            return
+    posts = sub.hot(limit=100)
     url_list = []
-
     for post in posts:
         url_list.append(post.url)
-
     out_tex = random.choice(url_list)
     await cmd.bot.send_message(message.channel, out_tex)
