798145592e90ca98221db26b5fb77c0d9e455acb
AXAz0r <thealeksaradovic@gmail.com>
2017-01-26 03:34:12 +0100

    Prune Fix and Embedding

--- sigma/plugins/moderation/prune.py
+++ sigma/plugins/moderation/prune.py
@@ -1,34 +1,31 @@
+import discord
 from sigma.core.permission import check_man_msg
-import asyncio
 
 
 async def prune(cmd, message, args):
     channel = message.channel
     if check_man_msg(message.author, channel):
-        try:
-            if args[0]:
-                count = int(args[0])
-            else:
-                count = 100
-        except:
-            count = 100
-        try:
-            if args[1]:
-                def crit(m):
-                    return m.author == message.mentions[0]
-            else:
-                crit = None
-        except:
-            crit = None
-        try:
-            await cmd.bot.purge_from(message.channel, limit=count, check=crit)
-            response = await cmd.bot.send_message(message.channel, 'Done! :ok_hand:')
-            await asyncio.sleep(10)
-            await cmd.bot.delete_message(response)
-        except Exception as e:
-            cmd.log.error(e)
-            await cmd.bot.send_message(message.channel, str(e))
+        limit = 100
+        target = cmd.bot.user
+        if not args:
+            limit = 100
+            target = cmd.bot.user
+        if len(args) == 1 and message.mentions:
+            limit = 100
+            target = message.mentions[0]
+        if len(args) > 1 and message.mentions:
+            target = message.mentions[0]
+            limit = abs(int(args[0]))
+        if len(args) == 1 and not message.mentions:
+            target = None
+            limit = abs(int(args[0]))
+
+        def author_check(msg):
+            return msg.author == target
+
+        deleted = await cmd.bot.purge_from(message.channel, limit=limit, check=author_check)
+        embed = discord.Embed(color=0x66CC66, title=':white_check_mark: Deleted ' + str(len(deleted)) + ' Messages')
     else:
-        response = await cmd.bot.send_message(message.channel, 'Only a user with the **Manage Messages** privilege can use this command. :x:')
-        await asyncio.sleep(10)
-        await cmd.bot.delete_message(response)
+        embed = discord.Embed(title=':warning: Unpermitted. Only Those With The Manage Message Permission Allowed.',
+                              color=0xDB0000)
+    await cmd.bot.send_message(message.channel, None, embed=embed)
