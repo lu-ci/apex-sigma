29c69e07e2dbe3663bee5d4b5809e6a0abd6011a
AXAz0r <thealeksaradovic@gmail.com>
2017-02-03 17:53:37 +0100

    SBO Fix For New Hierarchy

--- sigma/plugins/joke/sbo.py
+++ sigma/plugins/joke/sbo.py
@@ -9,13 +9,18 @@ async def sbo(cmd, message, args):
         embed = discord.Embed(color=0xDB0000, title=':exclamation: No sound given.')
         await cmd.bot.send_message(message.channel, None, embed=embed)
         return
-    sound_name = ' '.join(args)
-    if sound_name not in os.listdir(cmd.resource('sounds/')):
+    sounds_path = ' '.join(args)
+    sound_name = sounds_path.split('/')[-1]
+    sounds = []
+    for root, dirs, files in os.walk(cmd.resource('sounds/')):
+        for file in files:
+            sounds.append(file)
+    if sound_name not in sounds:
         embed = discord.Embed(color=0xDB0000, title=':exclamation: Sound not found.')
         await cmd.bot.send_message(message.channel, None, embed=embed)
         return
     voice = await cmd.bot.join_voice_channel(message.author.voice_channel)
-    player = voice.create_ffmpeg_player(cmd.resource('sounds/' + sound_name))
+    player = voice.create_ffmpeg_player(cmd.resource('sounds/' + sounds_path))
     player.start()
     while not player.is_done():
         asyncio.sleep(1)
