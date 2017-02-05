3ab69bea98a061f1fc6dc202ff0191854551aaa5
AXAz0r <thealeksaradovic@gmail.com>
2017-02-05 05:09:27 +0100

    Give Embed

--- sigma/plugins/misc/give.py
+++ sigma/plugins/misc/give.py
@@ -1,3 +1,6 @@
+import discord
+
+
 async def give(cmd, message, args):
     if not args:
         await cmd.bot.send_message(message.channel, cmd.help())
@@ -10,17 +13,17 @@ async def give(cmd, message, args):
         return
     target_user = message.mentions[0]
     if target_user.bot:
-        await cmd.bot.send_message(message.channel, 'You can not give points to bots.')
         return
     amount = abs(int(args[0]))
     curr_points = cmd.db.get_points(message.server, message.author)
     if amount > curr_points:
-        await cmd.bot.send_message(message.channel,
-                                   'You don\'t seem have enough points, <@' + message.author.id + '>...\nYou wanted to transfer **' + str(
-                                       amount) + '** while you only have **' + str(curr_points) + '**.')
+        out_content = discord.Embed(type='rich', color=0xDB0000,
+                                    title=':no_entry: You Do Not Have Enough Points.')
+        await cmd.bot.send_message(message.channel, None, embed=out_content)
         return
     else:
         cmd.db.take_points(message.server, message.author, amount)
         cmd.db.add_points(message.server, target_user, amount)
-        await cmd.bot.send_message(message.channel, 'Alright <@' + message.author.id + '>, I\'ve transfered **' + str(
-            amount) + '** points to <@' + target_user.id + '>!')
+        out_content = discord.Embed(type='rich', color=0x66CC66,
+                                    title=':white_check_mark: Points Transferred.')
+        await cmd.bot.send_message(message.channel, None, embed=out_content)
