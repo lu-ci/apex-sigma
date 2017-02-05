471519d1a4201129aecedfa7a827f7e0e4450bf4
AXAz0r <thealeksaradovic@gmail.com>
2017-02-05 05:07:39 +0100

    UnBan Embed

--- sigma/plugins/moderation/unban.py
+++ sigma/plugins/moderation/unban.py
@@ -1,5 +1,5 @@
 from sigma.core.permission import check_ban
-import asyncio
+import discord
 
 
 async def unban(cmd, message, args):
@@ -8,24 +8,24 @@ async def unban(cmd, message, args):
         user_q = args[0]
         if message.author is not user_q:
             if check_ban(message.author, channel):
-                try:
-                    ban_list = await cmd.bot.get_bans(message.server)
-                    target_user = None
-                    for user in ban_list:
-                        if user.name.lower() == user_q.lower():
-                            target_user = user
-                            break
-                    if target_user:
-                        await cmd.bot.unban(message.server, target_user)
-                        await cmd.bot.send_message(message.channel, 'User **' + user_q + '** has been unbanned.')
-                    else:
-                        await cmd.bot.send_message(message.channel, 'A user with that name was not found in the server ban list.')
-                except SyntaxError as e:
-                    cmd.log.error(e)
-                    await cmd.bot.send_message(message.channel, str(e))
+                ban_list = await cmd.bot.get_bans(message.server)
+                target_user = None
+                for user in ban_list:
+                    if user.name.lower() == user_q.lower():
+                        target_user = user
+                        break
+                if target_user:
+                    await cmd.bot.unban(message.server, target_user)
+                    out_content = discord.Embed(type='rich', color=0x66CC66,
+                                                title=':white_check_mark: ' + target_user.name + 'Unbanned.')
+                    await cmd.bot.send_message(message.channel, None, embed=out_content)
+                else:
+                    out_content = discord.Embed(type='rich', color=0xFF9900,
+                                                title=':warning: User Not Found In Ban List.')
+                    await cmd.bot.send_message(message.channel, None, embed=out_content)
             else:
-                response = await cmd.bot.send_message(message.channel, 'Only a user with **Ban** privileges can use this command. :x:')
-                await asyncio.sleep(10)
-                await cmd.bot.delete_message(response)
+                out_content = discord.Embed(type='rich', color=0xDB0000,
+                                            title=':no_entry: Insufficient Permissions. Ban Permission Required.')
+                await cmd.bot.send_message(message.channel, None, embed=out_content)
     else:
         await cmd.bot.send_message(message.channel, cmd.help())
