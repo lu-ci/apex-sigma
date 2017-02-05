9802c48c95bf04471a51fc217eb3a2a2c4794081
AXAz0r <thealeksaradovic@gmail.com>
2017-02-05 05:20:44 +0100

    Unflip Embed

--- sigma/plugins/moderation/unflip.py
+++ sigma/plugins/moderation/unflip.py
@@ -1,14 +1,18 @@
 from sigma.core.permission import check_admin
-
+import discord
 
 async def unflip(cmd, message, args):
     if check_admin(message.author, message.channel):
         active = cmd.db.get_settings(message.server.id, 'Unflip')
         if active:
             cmd.db.set_settings(message.server.id, 'Unflip', False)
-            await cmd.bot.send_message(message.channel, 'Unflip settings toggled to **Disabled**')
+            out_content = discord.Embed(type='rich', color=0x66CC66,
+                                        title=':white_check_mark: Automatic Table Un-Flipping Disabled')
         else:
             cmd.db.set_settings(message.server.id, 'Unflip', True)
-            await cmd.bot.send_message(message.channel, 'Unflip settings toggled to **Enabled**')
+            out_content = discord.Embed(type='rich', color=0x66CC66,
+                                        title=':white_check_mark: Automatic Table Un-Flipping Enabled')
     else:
-        await cmd.bot.send_message(message.channel, 'Insufficient permissions.')
+        out_content = discord.Embed(type='rich', color=0xDB0000,
+                                    title=':no_entry: Insufficient Permissions. Server Admin Only.')
+    await cmd.bot.send_message(message.channel, None, embed=out_content)
