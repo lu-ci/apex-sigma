fd05ab70e57c41b649287531a01f599a1e81e1b8
AXAz0r <thealeksaradovic@gmail.com>
2017-02-09 06:20:32 +0100

    Dog Command Fix

--- sigma/plugins/fun/dog.py
+++ sigma/plugins/fun/dog.py
@@ -8,6 +8,6 @@ async def dog(cmd, message, args):
     doggie_url = 'http://www.randomdoggiegenerator.com/randomdoggie.php'
     doggie_image = requests.get(doggie_url).content
     with Image.open(BytesIO(doggie_image)) as img:
-        img.save('cache/pupper_' + message.author.id + '.png')
+        img.save('cache/pupper_' + message.id + '.png')
     await cmd.bot.send_file(message.channel, 'cache/pupper_' + message.author.id + '.png')
     os.remove('cache/pupper_' + message.id + '.png')
