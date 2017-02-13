749be4155f14edb7bbfcd3a15269550a8e17aa34
AXAz0r <thealeksaradovic@gmail.com>
2017-02-13 12:51:20 +0100

    Permission File Cleaning and PM Redirect Minify
    And Grab Attachments if Any

--- sigma/plugins/administration/pmredirect.py
+++ sigma/plugins/administration/pmredirect.py
@@ -11,13 +11,13 @@ async def pmredirect(ev, message, args):
             return
         else:
             ev.log.info('User {:s} [{:s}] sent a private message.'.format(author.name, author.id))
-
-            # very expensive operation
-            for user in ev.bot.get_all_members():
-                if user.id == permitted_id[0]:
-                    embed = discord.Embed(title=':information_source: Message Recieved', color=0x0099FF)
-                    embed.add_field(
-                        name=message.author.name + '#' + message.author.discriminator + ' (' + message.author.id + ')',
-                        value=message.content)
-                    await ev.bot.send_message(user, None, embed=embed)
-                    break
+            embed = discord.Embed(color=0x0099FF)
+            embed.add_field(name='{:s} Sent a Message'.format(author.name), value='```\n' + ' '.join(args) + '\n```')
+            embed.set_footer(text=author.id)
+            if message.attachments:
+                attachment_links = ''
+                for attachment in message.attachments:
+                    attachment_links += '\n' + attachment.url
+                embed.add_field(name='Attachment', value=attachment_links)
+            owner = discord.utils.find(lambda usr: usr.id == permitted_id[0], ev.bot.get_all_members())
+            await ev.bot.send_message(owner, None, embed=embed)
