e53805a8cf52eb8dcb63277d7e428c0d63b028f3
AXAz0r <thealeksaradovic@gmail.com>
2017-02-21 15:41:58 +0100

    Remove Color Log Cause Who Needs It?

--- sigma/core/logger.py
+++ sigma/core/logger.py
@@ -2,12 +2,11 @@ import os
 from time import time
 from datetime import datetime as date
 import logging
-import colorlog
 
-log_fmt = '%(log_color)s%(levelname)-6s %(asctime)s %(name)-20s %(message)s'
+log_fmt = '%(levelname)-8s %(asctime)s %(name)-20s %(message)s'
 
 if os.getenv('LOGTARGET_JOURNAL'):
-    log_fmt = '%(levelname)-6s %(name)-20s %(message)s'
+    log_fmt = '%(levelname)-8s %(name)-20s %(message)s'
 
 log_dir = 'log'
 
@@ -17,14 +16,11 @@ if not os.path.exists(log_dir):
 logfile_name = date.fromtimestamp(time()).strftime('%Y%m%d-%H%M%S') + '.log'
 log_file = os.path.join(log_dir, logfile_name)
 
-# formatter = logging.Formatter(log_fmt)
-formatter = colorlog.ColoredFormatter(log_fmt)
+formatter = logging.Formatter(log_fmt)
 
 
 def create_logger(name):
-    # stream_handler = logging.StreamHandler()
-    # stream_handler.setFormatter(formatter)
-    stream_handler = colorlog.StreamHandler()
+    stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
 
     file_handler = logging.FileHandler(log_file, encoding='utf-8')
