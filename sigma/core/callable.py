73054e663a414932f19571d7a6af3c29879f2c38
AXAz0r <thealeksaradovic@gmail.com>
2017-02-24 17:53:41 +0100

    Extra Call Exception Verbosity

--- sigma/core/callable.py
+++ sigma/core/callable.py
@@ -91,7 +91,7 @@ class Callable(object):
                 title = ':exclamation: An Error Occurred!'
                 errmsg = 'For more information you can go to the AP Discord server and ask us, '
                 errmsg += 'the link is in the help.'
-                self.log.error(str(e))
+                self.log.error(f'CMD: {self.name} | ERROR: {str(e)}')
                 error_embed = discord.Embed(color=0xDB0000)
                 error_embed.add_field(name=title, value=codeblock(str(e)))
                 error_embed.set_footer(text=errmsg)
