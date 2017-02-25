41d53ecaf2ad4fff8c7ff2b73a5ed1079f520317
AXAz0r <thealeksaradovic@gmail.com>
2017-02-25 13:21:49 +0100

    Cat Uses AIO

--- sigma/plugins/fun/cat.py
+++ sigma/plugins/fun/cat.py
@@ -1,4 +1,4 @@
-import requests
+import aiohttp
 from config import CatAPIKey
 from lxml import html
 import random
@@ -6,8 +6,10 @@ import discord
 
 
 async def cat(cmd, message, args):
-    raw_page = requests.get('http://thecatapi.com/api/images/get?format=xml&results_per_page=100&api_key=' + CatAPIKey)
-    results = html.fromstring(raw_page.content)[0][0]
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+                'http://thecatapi.com/api/images/get?format=xml&results_per_page=100&api_key=' + CatAPIKey) as raw_page:
+            results = html.fromstring(await raw_page.text())[0][0]
     choice = random.choice(results)
     image_url = str(choice[0].text)
     embed = discord.Embed(color=0x1abc9c)
