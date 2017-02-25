a8f2701127af41ab354277cecd0dd914b4558e0b
AXAz0r <thealeksaradovic@gmail.com>
2017-02-25 13:18:24 +0100

    Bash Made To Use AIOHTTP

--- sigma/plugins/fun/bash.py
+++ sigma/plugins/fun/bash.py
@@ -1,27 +1,30 @@
 from discord import Embed
+import lxml.html as l
+import aiohttp
 
 cache = []
 
 async def bash(cmd, message, args):
     if len(cache) == 0:
-        import lxml.html as l, requests
-
-        page = requests.get('http://bash.org/?random1')
-        quotes = l.fromstring(page.content).cssselect('body center table tr td[valign="top"]')[0]
+        async with aiohttp.ClientSession() as session:
+            async with session.get('http://bash.org/?random1') as page:
+                page = await page.text()
+                quotes = l.fromstring(page).cssselect('body center table tr td[valign="top"]')[0]
         for index in range(1, len(quotes), 2):
-            id = quotes[index - 1][0][0].text
+            qid = quotes[index - 1][0][0].text
             score = quotes[index - 1][2].text
             quote = quotes[index].text_content()
 
             quote = {
-                'id': id[1:],
+                'id': qid[1:],
                 'score': score,
                 'quote': quote
             }
             cache.append(quote)
 
     quote = cache.pop()
-    embed = Embed(type='rich', color=0XC08000, title=':scroll: A Bash Quote', description='```xml\n{}```'.format(quote['quote']))
+    text = quote['quote']
+    embed = Embed(type='rich', color=0XC08000, title=':scroll: A Bash Quote', description=f'```xml\n{text}\n```')
     embed.set_footer(text='ID: {} | Score: {}'.format(quote['id'], quote['score']))
 
     await cmd.bot.send_message(message.channel, None, embed=embed)
