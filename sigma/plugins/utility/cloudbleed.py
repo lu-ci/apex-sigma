41c6e0b75552a8bbb0a4d23063aaa1fa99e66f9d
AXAz0r <thealeksaradovic@gmail.com>
2017-02-26 15:31:12 +0100

    CloudBleed AIO and API Change

--- sigma/plugins/utility/cloudbleed.py
+++ sigma/plugins/utility/cloudbleed.py
@@ -1,16 +1,18 @@
 import discord
-import requests
+import aiohttp
 
 
 async def cloudbleed(cmd, message, args):
     if args:
-        url = 'https://cloudbleed-api.whats-th.is/api/v1/search?query=' + ' '.join(args)
-        data = requests.get(url).json()
-        if len(data['items']) == 0:
+        url = 'https://api.auroraproject.xyz/api/v1/cloudbleed?qry=' + ' '.join(args)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as data:
+                data = await data.json()
+        if len(data['urls']) == 0:
             embed = discord.Embed(color=0x66CC66, title=':white_check_mark: Not Found')
             embed.set_footer(text='This is a good thing.')
         else:
-            result_text = ', '.join(data['items'][:20])
+            result_text = ', '.join(data['urls'][:20])
             embed = discord.Embed(color=0xf48220)
             embed.add_field(name=':syringe: Websites found matching that search.', value=f'```\n{result_text}\n```')
         await cmd.bot.send_message(message.channel, None, embed=embed)
