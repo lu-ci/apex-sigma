a0f1e25cd9c3d88a2699b3bf2c06b39f50917357
AXAz0r <thealeksaradovic@gmail.com>
2017-03-03 20:51:22 +0100

    Fixed Fucking Location Things YAY

--- sigma/plugins/games/warframe/nodes/read_frame_data.py
+++ sigma/plugins/games/warframe/nodes/read_frame_data.py
@@ -9,12 +9,12 @@ async def read_frame_data(url):
             page = await data.text()
     root = l.fromstring(page)
     drop_tables = root.cssselect('.tabbertab')
-    pc_drop_table = drop_tables[1][1]
+    pc_drop_table = drop_tables[0][1]
     item_image = root.cssselect('.pi-image-thumbnail')[0].attrib['src']
     embed = discord.Embed(color=0x0066CC)
     embed.set_thumbnail(url=item_image)
     for row in pc_drop_table:
-        row_title = row[0].text
+        row_title = row[0].text.strip()
         row_location = "\n".join([x for x in row[1].itertext()])
         embed.add_field(name=row_title, value=f'```\n{row_location}\n```')
     return embed
@@ -26,7 +26,7 @@ async def read_item_data(url):
             page = await data.text()
     root = l.fromstring(page)
     drop_tables = root.cssselect('.tabbertab')
-    pc_drop_table = drop_tables[1][1]
+    pc_drop_table = drop_tables[0][1]
     item_image = root.cssselect('.infobox')[0][1][0][0].attrib['href']
     embed = discord.Embed(color=0x0066CC)
     embed.set_thumbnail(url=item_image)
