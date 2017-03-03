b6e33e77ceb449b17b1eea8e269f6fd96133f8e3
AXAz0r <thealeksaradovic@gmail.com>
2017-03-03 17:46:12 +0100

    Updated The Version Files and Names

--- sigma/plugins/music/disconnect.py
+++ sigma/plugins/music/disconnect.py
@@ -7,7 +7,7 @@ async def disconnect(cmd, message, args):
             title=':warning: I don\'t see you in a voice channel', color=0xFF9900)
         await cmd.bot.send_message(message.channel, None, embed=embed)
         return
-    await cmd.bot.disconnect()
     voice = cmd.bot.voice_client_in(message.server)
+    await voice.disconnect()
     embed = discord.Embed(color=0x66CC66, title=f':white_check_mark: Disconnected From {voice.channel.name}')
     await cmd.bot.send_message(message.channel, None, embed=embed)
--- sigma/plugins/music/playlist_adder.py
+++ sigma/plugins/music/playlist_adder.py
@@ -2,11 +2,14 @@ import arrow
 
 
 def playlist_adder(sid, music, req, playlist_obj):
+    counter = 0
     for item in playlist_obj:
-        data = {
-            'url': 'https://www.youtube.com/watch?v=' + item.videoid,
-            'requester': req,
-            'video': item,
-            'timestamp': arrow.now().timestamp
-        }
-        music.add_to_queue(sid, data)
+        if counter < 500:
+            data = {
+                'url': 'https://www.youtube.com/watch?v=' + item.videoid,
+                'requester': req,
+                'video': item,
+                'timestamp': arrow.now().timestamp
+            }
+            music.add_to_queue(sid, data)
+            counter += 1
