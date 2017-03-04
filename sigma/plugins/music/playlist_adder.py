b89c706491d0d94716968c8bb96334fbcf2c48fe
AXAz0r <thealeksaradovic@gmail.com>
2017-03-04 17:59:08 +0100

    Small Nothinglike Fixes

--- sigma/plugins/music/playlist_adder.py
+++ sigma/plugins/music/playlist_adder.py
@@ -2,17 +2,12 @@ import arrow
 
 
 async def playlist_adder(sid, music, req, playlist_obj):
-    counter = 0
     for item in playlist_obj['items']:
         item = item['pafy']
-        if counter < 100:
-            data = {
-                'url': 'https://www.youtube.com/watch?v=' + item.videoid,
-                'requester': req,
-                'video': item,
-                'timestamp': arrow.now().timestamp
-            }
-            music.add_to_queue(sid, data)
-            counter += 1
-        else:
-            break
+        data = {
+            'url': 'https://www.youtube.com/watch?v=' + item.videoid,
+            'requester': req,
+            'video': item,
+            'timestamp': arrow.now().timestamp
+        }
+        music.add_to_queue(sid, data)
