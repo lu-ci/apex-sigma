37564bb0d21e51c9c03ba5fd350ecab4dc959028
AXAz0r <thealeksaradovic@gmail.com>
2017-03-05 21:10:14 +0100

    Response with correct Help command and fStrings

--- sigma/plugins/administration/pmredirect.py
+++ sigma/plugins/administration/pmredirect.py
@@ -1,5 +1,6 @@
 import discord
-from config import permitted_id
+from config import permitted_id, Prefix
+from sigma.core.utils import user_avatar
 
 
 async def pmredirect(ev, message, args):
@@ -9,17 +10,23 @@ async def pmredirect(ev, message, args):
         if author.id == cid or author.id in permitted_id:
             return
         else:
-            ev.log.info('User {:s} [{:s}] sent a private message.'.format(author.name, author.id))
+            if not message.content.startswith(Prefix) and author.id not in permitted_id:
+                pm_response = discord.Embed(color=0x0099FF, title=f'ℹ Type `{Prefix}help` for info on how to use me!')
+                await ev.bot.send_message(message.channel, None, embed=pm_response)
+            ev.log.info(f'User {author.name} [{author.id}] sent a private message.')
             embed = discord.Embed(color=0x0099FF)
             if args and not ''.join(args) == '':
                 embed.add_field(name='Message',
                                 value='```\n' + ' '.join(args) + '\n```', inline=False)
-            embed.set_footer(text='UserID: ' + author.id)
-            embed.set_author(name='{:s}#{:s}:'.format(author.name, author.discriminator), icon_url=author.avatar_url)
+            embed.set_footer(text=f'UserID: {author.id}')
+            embed.set_author(name=f'{author.name}#{author.discriminator}', icon_url=user_avatar(author))
             if message.attachments:
                 attachment_links = ''
                 for attachment in message.attachments:
-                    attachment_links += '\n' + attachment['url']
+                    if attachment['url'].split('.')[-1] in ['png', 'jpg', 'jpeg', 'gif']:
+                        embed.set_image(url=attachment['url'])
+                    else:
+                        attachment_links += '\n' + attachment['url']
                 embed.add_field(name='Attachments', value=attachment_links, inline=False)
             owner = discord.utils.find(lambda usr: usr.id == permitted_id[0], ev.bot.get_all_members())
             await ev.bot.send_message(owner, None, embed=embed)
