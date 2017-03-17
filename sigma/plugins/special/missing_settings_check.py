8bbb438af0c1e92d5456017b06617cb5795088c2
AXAz0r <thealeksaradovic@gmail.com>
2017-03-17 06:55:28 +0100

    Setting Check Now In Core

--- sigma/plugins/special/missing_settings_check.py
+++
@@ -1,5 +0,0 @@
-async def missing_settings_check(ev):
-    ev.log.info('Checking Missing Settings')
-    for server in ev.bot.servers:
-        ev.db.check_for_missing_settings(server)
-    ev.log.info('Settings Check Complete')
