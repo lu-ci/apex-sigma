f7ab2584673bd58f2716b88910980d0dfb10d636
AXAz0r <thealeksaradovic@gmail.com>
2017-03-19 08:17:02 +0100

    Separate Greet and Bye Channels To Own Commands

---
+++ sigma/plugins/moderation/greetch.py
@@ -0,0 +1,15 @@
+import discord
+from sigma.core.permission import check_admin
+
+
+async def greetch(cmd, message, args):
+    if not check_admin(message.author, message.channel):
+        embed = discord.Embed(title='⛔ Unpermitted. Server Admin Only.', color=0xDB0000)
+    else:
+        if message.channel_mentions:
+            target_channel = message.channel_mentions[0]
+        else:
+            target_channel = message.channel
+        cmd.db.set_settings(message.server.id, 'GreetChannel', target_channel.id)
+        embed = discord.Embed(color=0x66CC66, title=f'✅ Greeting Channel Changed To {target_channel.name}')
+    await cmd.bot.send_message(message.channel, None, embed=embed)
