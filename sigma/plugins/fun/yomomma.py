61b9ce84ec32a6c87c22972f090d31220e5ad98b
AXAz0r <thealeksaradovic@gmail.com>
2017-03-22 18:06:51 +0100

    Unexpected Json Fix Atempt

--- sigma/plugins/fun/yomomma.py
+++ sigma/plugins/fun/yomomma.py
@@ -1,12 +1,13 @@
 import aiohttp
 import discord
-
+import json
 
 async def yomomma(cmd, message, args):
     resource = 'http://api.yomomma.info/'
     async with aiohttp.ClientSession() as session:
         async with session.get(resource) as data:
-            data = await data.json()
+            data = await data.read()
+            data = json.loads(data)
     joke = data['joke']
     if not joke.endswith('.'):
         joke += '.'
