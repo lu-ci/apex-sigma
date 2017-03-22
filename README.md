5f7740d381c03f16efd44f6a0aec5b9d3bf1643a
AXAz0r <thealeksaradovic@gmail.com>
2017-03-22 13:46:25 +0100

    Blacklist Listing Command

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 [![apex](https://i.imgur.com/TRSdGni.png)](https://auroraproject.xyz/)
 
 <p align="center">
-<img src="https://img.shields.io/badge/Sigma-2.34-1B6F5F.svg?style=flat-square" />
-<img src="https://img.shields.io/badge/Codename-Yukine-1B6F5F.svg?style=flat-square" />
+<img src="https://img.shields.io/badge/Sigma-2.35-1B6F5F.svg?style=flat-square" />
+<img src="https://img.shields.io/badge/Codename-Ryou-1B6F5F.svg?style=flat-square" />
 <img src="https://img.shields.io/badge/License-GPLv3-red.svg?style=flat-square" />
 </p>
 <p align="center">
---
+++ sigma/plugins/moderation/blacklist.py
@@ -0,0 +1,23 @@
+import discord
+
+
+async def blacklist(cmd, message, args):
+    black_users = cmd.db.get_settings(message.server.id, 'BlacklistedUsers')
+    black_chnls = cmd.db.get_settings(message.server.id, 'BlacklistedChannels')
+    if not black_chnls and not black_users:
+        response = discord.Embed(color=0x0099FF, title='ℹ Nothing Is Blacklisted Here')
+    else:
+        response = discord.Embed(color=0x1abc9c)
+        if black_chnls:
+            black_chnls_list = []
+            for chnl in black_chnls:
+                chnl_name = discord.utils.find(lambda x: x.id == chnl, cmd.bot.get_all_channels()).name
+                black_chnls_list.append(chnl_name)
+            response.add_field(name='Blacklisted Channels', value=f'```\n{", ".join(black_chnls_list)}\n```')
+        if black_users:
+            black_users_list = []
+            for usr in black_users:
+                chnl_name = discord.utils.find(lambda x: x.id == usr, cmd.bot.get_all_members()).name
+                black_users_list.append(f'#{chnl_name}')
+            response.add_field(name='Blacklisted Users', value=f'```\n{", ".join(black_users_list)}\n```')
+    await cmd.bot.send_message(message.channel, None, embed=response)
