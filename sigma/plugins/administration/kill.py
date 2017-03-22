f5c768224046bf930c9d95b6971cd7cebae83f8b
AXAz0r <thealeksaradovic@gmail.com>
2017-03-22 14:06:39 +0100

    Quick Kill Fix

--- sigma/plugins/administration/kill.py
+++ sigma/plugins/administration/kill.py
@@ -6,6 +6,6 @@ async def kill(cmd, message, args):
     if message.author.id in permitted_id:
         status = discord.Embed(title=':skull_crossbones: Sigma Shutting Down.', color=0x808080)
         await cmd.bot.send_message(message.channel, None, embed=status)
-        await cmd.bot.logout()
         cmd.log.info('Terminated by user {:s}'.format(message.author.name))
-        exit('Terminated by command.')
+        await cmd.bot.logout()
+        await cmd.bot.close()
