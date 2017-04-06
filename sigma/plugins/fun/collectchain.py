ad54dbdf4d12e22c7ebab1c7c96811fbc42becd1
AXAz0r <thealeksaradovic@gmail.com>
2017-04-06 06:18:08 +0200

    Better Send Again Than Edit

--- sigma/plugins/fun/collectchain.py
+++ sigma/plugins/fun/collectchain.py
@@ -25,7 +25,7 @@ async def collectchain(cmd, message, args):
                 in_use = True
                 ch_response = discord.Embed(color=0x66CC66,
                                             title='📖 Collecting... You will be sent a DM when I\'m done.')
-                ch_res_msg = await cmd.bot.send_message(message.channel, None, embed=ch_response)
+                await cmd.bot.send_message(message.channel, None, embed=ch_response)
                 async for log in cmd.bot.logs_from(def_chn, limit=50000):
                     if log.author.id == target.id:
                         if log.content:
@@ -57,7 +57,7 @@ async def collectchain(cmd, message, args):
                 dm_response.add_field(name='Amount Collected', value=f'```\n{collected}\n```')
                 dm_response.add_field(name='Time Elapsed', value=f'```\n{arrow.utcnow().timestamp - start_time}\n```')
                 await cmd.bot.send_message(message.author, None, embed=dm_response)
-                await cmd.bot.edit_message(ch_res_msg, None, embed=ch_response.set_footer(text='All Done!'))
+                await cmd.bot.send_message(message.channel, None, embed=dm_response)
                 if message.author != target:
                     tgt_msg = discord.Embed(color=0x66CC66,
                                             title=f'📖 {message.author.name} has made a markov chain for you.')
