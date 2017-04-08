1b65a2178b4511d364c9f10240b02ef96fa0158a
AXAz0r <thealeksaradovic@gmail.com>
2017-04-08 05:41:20 +0200

    Points Node Reworked

--- sigma/core/db_nodes/points.py
+++ sigma/core/db_nodes/points.py
@@ -1,6 +1,41 @@
+import arrow
+
+
+def default_data(server, user, points, add):
+    if add:
+        total_pts = points
+    else:
+        total_pts = 0
+    data = {
+        'UserID': user.id,
+        'Total': total_pts,
+        'Servers': {server.id: total_pts}
+    }
+    return data
+
+
 def point_manipulation(db, server, user, points, add):
     collection = 'PointSystem'
+    data = db[collection].find_one({'UserID': user.id})
+    if data:
+        total_pts = data['Total']
+        servers = data['Servers']
+        if server.id in servers:
+            if add:
+                new_pts = servers[server.id] + points
+                total_pts += points
+            else:
+                new_pts = servers[server.id] - points
+        else:
+            new_pts = points
+        servers.update({server.id: new_pts})
+        db[collection].update_one({'UserID': user.id}, {'$set': {'Servers': servers, 'Total': total_pts}})
+    else:
+        data = default_data(server, user, points, add)
+        db[collection].insert_one(data)
 
 
-def point_grabber(db, server, user):
+def point_grabber(db, user):
     collection = 'PointSystem'
+    data = db[collection].find_one({'UserID': user.id})
+    return data
--- sigma/plugins/points/reward.py
+++ sigma/plugins/points/reward.py
@@ -7,3 +7,4 @@ async def reward(ev, message, args):
             if not ev.cooldown.on_cooldown(ev, message):
                 points = random.randint(3, 15)
                 ev.db.add_points(message.server, message.author, points)
+                ev.cooldown.set_cooldown(ev, message, 60)
