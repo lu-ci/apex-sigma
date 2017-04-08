ce5e06e596826eced1013162ea898d1801c9a24a
AXAz0r <thealeksaradovic@gmail.com>
2017-04-08 06:18:48 +0200

    Sender and Receiver Fields Proper

--- sigma/plugins/administration/takepts.py
+++ sigma/plugins/administration/takepts.py
@@ -23,13 +23,13 @@ async def takepts(cmd, message, args):
                 return
             cmd.db.take_points(message.server, target, amount)
             out = discord.Embed(title='✅ Done', color=0x66CC66)
-            out.add_field(name='Taken From', value=target.name + '#' + target.discriminator)
+            out.add_field(name='Taken From', value=f'```py\n{target.name}#{target.discriminator}\n```')
             out.add_field(name='Amount', value=f'```py\n{amount} {Currency}\n```')
             await cmd.bot.send_message(message.channel, None, embed=out)
             try:
                 out = discord.Embed(title=f'🔥 {Currency} Were Taken Away', color=0xFF9900)
                 out.add_field(name='Server', value=message.server.name)
-                out.add_field(name='By', value=message.author.name + '#' + message.author.discriminator)
+                out.add_field(name='By', value=f'```py\n{message.author.name}#{message.author.discriminator}\n```')
                 out.add_field(name='Amount', value=f'```py\n{amount} {Currency}\n```')
                 await cmd.bot.send_message(target, None, embed=out)
             except:
