e9ba87cba684fc5e0d4564236be5eb17b1790d4e
AXAz0r <thealeksaradovic@gmail.com>
2017-04-08 06:15:37 +0200

    Give, Award and Take Changes

--- sigma/plugins/administration/takepts.py
+++ sigma/plugins/administration/takepts.py
@@ -1,44 +1,38 @@
-﻿from config import permitted_id
-from sigma.core.permission import check_admin
-import discord
+﻿import discord
+from config import Currency
 
 
 async def takepts(cmd, message, args):
     if message.server:
-        if message.author.id in permitted_id or check_admin(message.author, message.channel):
-            if message.mentions:
-                target = message.mentions[0]
-                if target.bot:
-                    out = discord.Embed(title='❗ Can\'t award bots.', color=0xDB0000)
-                    await cmd.bot.send_message(message.channel, None, embed=out)
-                    return
-                try:
-                    amount = abs(int(args[0]))
-                except:
-                    out = discord.Embed(title='❗ Invalid Input.', color=0xDB0000)
-                    await cmd.bot.send_message(message.channel, None, embed=out)
-                    return
-                current_points = cmd.db.get_points(message.server, target)
-                if current_points < amount:
-                    out = discord.Embed(title='❗ They don\'t have that many points.', color=0xDB0000)
-                    await cmd.bot.send_message(message.channel, None, embed=out)
-                    return
-                cmd.db.take_points(message.server, target, amount)
-                out = discord.Embed(title='✅ Done', color=0x66CC66)
-                out.add_field(name='Taken From', value=target.name + '#' + target.discriminator)
-                out.add_field(name='Amount', value=str(amount))
+        if message.mentions:
+            target = message.mentions[0]
+            if target.bot:
+                out = discord.Embed(title='❗ Can\'t award bots.', color=0xDB0000)
                 await cmd.bot.send_message(message.channel, None, embed=out)
-                try:
-                    out = discord.Embed(title=':fire: Points Were Taken Away', color=0xFF9900)
-                    out.add_field(name='Server', value=message.server.name)
-                    out.add_field(name='By', value=message.author.name + '#' + message.author.discriminator)
-                    out.add_field(name='Amount', value=str(amount))
-                    await cmd.bot.send_message(target, None, embed=out)
-                except:
-                    pass
-            else:
-                await cmd.bot.send_message(message.channel, cmd.help())
+                return
+            try:
+                amount = abs(int(args[0]))
+            except:
+                out = discord.Embed(title='❗ Invalid Input.', color=0xDB0000)
+                await cmd.bot.send_message(message.channel, None, embed=out)
+                return
+            current_points = cmd.db.get_points(message.server, target)['Current']
+            if current_points < amount:
+                out = discord.Embed(title=f'❗ They don\'t have that many {Currency}.', color=0xDB0000)
+                await cmd.bot.send_message(message.channel, None, embed=out)
+                return
+            cmd.db.take_points(message.server, target, amount)
+            out = discord.Embed(title='✅ Done', color=0x66CC66)
+            out.add_field(name='Taken From', value=target.name + '#' + target.discriminator)
+            out.add_field(name='Amount', value=f'{amount} {Currency}')
+            await cmd.bot.send_message(message.channel, None, embed=out)
+            try:
+                out = discord.Embed(title=f'🔥 {Currency} Were Taken Away', color=0xFF9900)
+                out.add_field(name='Server', value=message.server.name)
+                out.add_field(name='By', value=message.author.name + '#' + message.author.discriminator)
+                out.add_field(name='Amount', value=f'{amount} {Currency}')
+                await cmd.bot.send_message(target, None, embed=out)
+            except:
+                pass
         else:
-            status = discord.Embed(type='rich', color=0xDB0000,
-                                   title='⛔ Insufficient Permissions. Bot Owner or Server Admin Only.')
-            await cmd.bot.send_message(message.channel, None, embed=status)
+            await cmd.bot.send_message(message.channel, cmd.help())
--- sigma/plugins/points/give.py
+++ sigma/plugins/points/give.py
@@ -1,5 +1,5 @@
 ﻿import discord
-
+from config import Currency
 
 async def give(cmd, message, args):
     if not args:
@@ -17,15 +17,13 @@ async def give(cmd, message, args):
     if target_user == message.author:
         return
     amount = abs(int(args[0]))
-    curr_points = cmd.db.get_points(message.server, message.author)
+    curr_points = cmd.db.get_points(message.author)['Current']
     if amount > curr_points:
-        out_content = discord.Embed(type='rich', color=0xDB0000,
-                                    title='⛔ You Do Not Have Enough Points.')
+        out_content = discord.Embed(color=0xDB0000, title=f'⛔ You Do Not Have Enough {Currency}.')
         await cmd.bot.send_message(message.channel, None, embed=out_content)
         return
     else:
         cmd.db.take_points(message.server, message.author, amount)
         cmd.db.add_points(message.server, target_user, amount)
-        out_content = discord.Embed(type='rich', color=0x66CC66,
-                                    title='✅ Points Transferred.')
+        out_content = discord.Embed(color=0x66CC66, title=f'✅ {Currency} Transferred.')
         await cmd.bot.send_message(message.channel, None, embed=out_content)
