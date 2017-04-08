e2593e2f3bc5c8ec8cdf8cb2285af140e0a3fc19
AXAz0r <thealeksaradovic@gmail.com>
2017-04-08 04:42:19 +0200

    Point Rework Started

--- sigma/core/db_nodes/points.py
+++ sigma/core/db_nodes/points.py
@@ -1,52 +1,6 @@
-def point_manipulation(db, server, user, points, point_type, add):
+def point_manipulation(db, server, user, points, add):
     collection = 'PointSystem'
-    finddata = {
-        'UserID': user.id,
-        'ServerID': server.id
-    }
-    insert_data = {
-        'UserID': user.id,
-        'ServerID': server.id,
-        'Points': 0,
-        'XP': 0,
-        'Level': 0
-    }
-    updatetarget = {"UserID": user.id, "ServerID": server.id}
-    search_res = db[collection].find_one(finddata)
-    # Check Existence
-    if not search_res:
-        db[collection].insert_one(insert_data)
-    else:
-        for key in insert_data:
-            if key not in search_res:
-                db[collection].update_one(updatetarget, {'$set': {key: insert_data[key]}})
-            else:
-                pass
-    data = db[collection].find_one(finddata)
-    old = data[point_type]
-    if add:
-        new = old + points
-    else:
-        new = old - points
-    db[collection].update_one(updatetarget, {'$set': {point_type: new}})
 
 
-def point_grabber(db, server, user, point_type):
+def point_grabber(db, server, user):
     collection = 'PointSystem'
-    finddata = {
-        'UserID': user.id,
-        'ServerID': server.id
-    }
-    insert_data = {
-        'UserID': user.id,
-        'ServerID': server.id,
-        'Points': 0,
-        'XP': 0,
-        'Level': 0
-    }
-    search_res = db[collection].find_one(finddata)
-    # Check Existence
-    if not search_res:
-        return insert_data[point_type]
-    else:
-        return search_res[point_type]
--- sigma/plugins/points/reward.py
+++ sigma/plugins/points/reward.py
@@ -2,14 +2,11 @@ import random
 
 
 async def reward(ev, message, args):
-    if message.server is None:
-        return
-    if message.author.bot:
-        return
-    cd_state = ev.db.on_cooldown(message.server.id, message.author.id, 'Activity', 60)
-    if not cd_state:
-        act_points = random.randint(20, 45)
-        points = random.randint(4, 12)
-        ev.db.add_points(message.server, message.author, points)
-        ev.db.add_act_points(message.server, message.author, act_points)
-        ev.db.set_cooldown(message.server.id, message.author.id, 'Activity')
+    if not message.author.bot:
+        if message.server:
+            if not cd_state:
+                act_points = random.randint(20, 45)
+                points = random.randint(4, 12)
+                ev.db.add_points(message.server, message.author, points)
+                ev.db.add_act_points(message.server, message.author, act_points)
+                ev.db.set_cooldown(message.server.id, message.author.id, 'Activity')
