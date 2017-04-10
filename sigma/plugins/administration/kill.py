7af0531737173697fcb5af033741dab210e25708
AXAz0r <thealeksaradovic@gmail.com>
2017-04-10 23:58:03 +0200

    Kill Try and Except

--- sigma/plugins/administration/kill.py
+++ sigma/plugins/administration/kill.py
@@ -5,7 +5,10 @@ from config import permitted_id
 async def kill(cmd, message, args):
     if message.author.id in permitted_id:
         status = discord.Embed(title=':skull_crossbones: Sigma Shutting Down.', color=0x808080)
-        await message.channel.send(None, embed=status)
+        try:
+            await message.channel.send(None, embed=status)
+        except:
+            pass
         cmd.log.info('Terminated by user {:s}'.format(message.author.name))
         await cmd.bot.logout()
         await cmd.bot.close()
