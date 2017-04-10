a79b0c9b8928c574109776f664bccedb4d0fe598
AXAz0r <thealeksaradovic@gmail.com>
2017-04-11 00:58:05 +0200

    Black SRV Fix

--- sigma/plugins/administration/blacksrv.py
+++ sigma/plugins/administration/blacksrv.py
@@ -1,31 +1,25 @@
-﻿from config import permitted_id
-import discord
+﻿import discord
 
 
 async def blacksrv(cmd, message, args):
-    if message.author.id in permitted_id:
-        if not args:
-            await message.channel.send(cmd.help())
-            return
-        target = None
-        for server in cmd.bot.guilds:
-            if server.id == args[0]:
-                target = server
-                break
-        if target:
-            black = cmd.db.get_settings(target.id, 'IsBlacklisted')
-            if black:
-                cmd.db.set_settings(target.id, 'IsBlacklisted', False)
-                embed = discord.Embed(title=':unlock: Server ' + target.name + ' has been un-blacklisted.',
-                                      color=0xFF9900)
-            else:
-                cmd.db.set_settings(target.id, 'IsBlacklisted', True)
-                embed = discord.Embed(title=':lock: Server ' + target.name + ' has been blacklisted.', color=0xFF9900)
+    if not args:
+        await message.channel.send(cmd.help())
+        return
+    target = None
+    for server in cmd.bot.guilds:
+        if server.id == int(args[0]):
+            target = server
+            break
+    if target:
+        black = cmd.db.get_settings(target.id, 'IsBlacklisted')
+        if black:
+            cmd.db.set_settings(target.id, 'IsBlacklisted', False)
+            embed = discord.Embed(title=':unlock: Server ' + target.name + ' has been un-blacklisted.',
+                                  color=0xFF9900)
         else:
-            embed = discord.Embed(type='rich', color=0xDB0000,
-                                  title='❗ No server by that ID was found.')
-        await message.channel.send(None, embed=embed)
+            cmd.db.set_settings(target.id, 'IsBlacklisted', True)
+            embed = discord.Embed(title=':lock: Server ' + target.name + ' has been blacklisted.', color=0xFF9900)
     else:
-        out_content = discord.Embed(type='rich', color=0xDB0000,
-                                    title='⛔ Insufficient Permissions. Bot Owner Only.')
-        await message.channel.send(None, embed=out_content)
+        embed = discord.Embed(type='rich', color=0xDB0000,
+                              title='❗ No server by that ID was found.')
+    await message.channel.send(None, embed=embed)
