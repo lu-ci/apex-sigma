a5beb3be339302cd103b5261005dc6f2948cd2b6
AXAz0r <thealeksaradovic@gmail.com>
2017-04-11 03:15:18 +0200

    Ranking Fix

--- sigma/plugins/points/ranking.py
+++ sigma/plugins/points/ranking.py
@@ -5,5 +5,5 @@ from config import MainServerURL
 async def ranking(cmd, message, args):
     embed = discord.Embed(color=0x1ABC9C)
     embed.add_field(name='Sigma Ranking for ' + message.guild.name,
-                    value='You can click [HERE](' + MainServerURL + 'ranking?sid=' + message.guild.id + ') to see the Sigma Ranking for this server.')
+                    value=f'You can click [HERE]({MainServerURL}ranking?sid={message.guild.id}) to see the Sigma Ranking for this server.')
     await message.channel.send(None, embed=embed)
