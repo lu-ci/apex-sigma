9c3467161188fe78ae653ed00aeac7f58beeec3c
AXAz0r <thealeksaradovic@gmail.com>
2017-04-11 03:17:22 +0200

    Accept Fix

--- sigma/plugins/special/accept.py
+++ sigma/plugins/special/accept.py
@@ -4,7 +4,7 @@ from config import permitted_id
 
 
 async def accept(ev, message, args):
-    if message.channel.id == '225375071349243904':
+    if message.channel.id == 225375071349243904:
         give_role = None
         for role in message.guild.roles:
             if role.name == 'Crabigator\'s Pet':
@@ -23,13 +23,13 @@ async def accept(ev, message, args):
                     embed = discord.Embed(
                         title='ℹ ' + message.author.name + ' has accepted the terms on WKCD.',
                         color=0x0099FF)
-                    await ev.bot.send_message(member, None, embed=embed)
+                    await member.send(None, embed=embed)
             for channel in message.guild.channels:
                 if channel.is_default:
-                    await ev.bot.send_message(channel,
-                                              'Hello <@' + message.author.id + '>! Welcome to the WaniKani Community Discord Server! If you wanna chat, feel bored, need help with your Nihongo or whatever crosses your mind, you are welcome to chat about it here~ If you encounter any problems be sure to say so to a moderator! The pinned messages in each channel will have some info as well\nようこそ！もし日本語でしゃべりたければ、音声チャネルにご参加ください！')
+                    await channel.send(
+                        'Hello <@' + message.author.id + '>! Welcome to the WaniKani Community Discord Server! If you wanna chat, feel bored, need help with your Nihongo or whatever crosses your mind, you are welcome to chat about it here~ If you encounter any problems be sure to say so to a moderator! The pinned messages in each channel will have some info as well\nようこそ！もし日本語でしゃべりたければ、音声チャネルにご参加ください！')
         elif message.content == '>>decline':
-            response = await ev.bot.send_message(message.channel,
+            response = await message.channel.send(
                 'We are sorry to hear that.\nYou will be removed from the server shortly.\nBon voyage~')
             await asyncio.sleep(5)
             await ev.bot.kick(message.author)
