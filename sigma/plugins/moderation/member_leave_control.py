269936d18b75a5a5ee1f7f1850b93b628325c149
AXAz0r <thealeksaradovic@gmail.com>
2017-04-11 22:58:50 +0200

    Join and Leave Message Fix

--- sigma/plugins/moderation/member_leave_control.py
+++ sigma/plugins/moderation/member_leave_control.py
@@ -1,4 +1,7 @@
-﻿async def member_leave_control(ev, member):
+﻿import discord
+
+
+async def member_leave_control(ev, member):
     server = member.guild
     bye = ev.db.get_settings(server.id, 'Bye')
     if bye:
@@ -7,8 +10,9 @@
         bye_message = ev.db.get_settings(server.id, 'ByeMessage')
         bye_message = bye_message.replace('%user_mention%', member.name).replace('%server_name%', server.name)
         if not bye_channel:
-            bye_channel = server.default_channel.id
-        target_channel = None
+            target_channel = server.default_channe
+        else:
+            target_channel = discord.utils.find(lambda x: x.id == bye_channel, member.guild.channels)
         for channel in server.channels:
             if channel.id == bye_channel:
                 target_channel = channel
