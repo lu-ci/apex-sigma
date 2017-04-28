a7a306fd74d941d9f62df30d1dd05bea8acb2d83
AXAz0r <thealeksaradovic@gmail.com>
2017-04-28 15:15:47 +0200

    Fixed Donors URL To Be Dynamic

--- sigma/plugins/utility/other/donors.py
+++ sigma/plugins/utility/other/donors.py
@@ -1,9 +1,9 @@
 ﻿import discord
-
+from config import MainServerURL
 
 async def donors(cmd, message, args):
     embed = discord.Embed(color=0x0099FF)
-    don_text = 'A **huge** thank you to [these](https://auroraproject.xyz#donors) lovely people!'
+    don_text = f'A **huge** thank you to [these]({MainServerURL}#donors) lovely people!'
     don_text += '\n\nWant to support us?\nWe have a [PayPal.me](https://www.paypal.me/AleksaRadovic) page!'
     embed.add_field(name=':gem: The lovely people that support us', value=don_text)
     embed.set_footer(text='Thank you! 🎀')
