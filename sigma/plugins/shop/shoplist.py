e8fe611dbaf4716725f14bd83d11a7a02a72a305
AXAz0r <thealeksaradovic@gmail.com>
2017-05-08 08:11:02 +0200

    Fixed Shop Listing

--- sigma/plugins/shop/shoplist.py
+++ sigma/plugins/shop/shoplist.py
@@ -9,12 +9,14 @@ async def shoplist(cmd, message, args):
     if len(shop_list) == 0:
         embed = discord.Embed(color=0xFF9900, title='⚠ The Shop Is Empty.')
     else:
-        embed = discord.Embed(color=0x1abc9c, title= ':gem: ' + message.guild.name + '\'s Role Shop')
+        embed = discord.Embed(color=0x1abc9c, title=':gem: ' + message.guild.name + '\'s Role Shop')
         role_out = ''
-        def pricecheck(item):
-            return int(item['Price'])
+
+        def pricecheck(itm):
+            return int(itm['Price'])
+
         shop_list = sorted(shop_list, key=pricecheck)
         for item in shop_list:
-            role_out += '\n```yml\n\'' + item['RoleName'] + '\'\n - ' + item['Price'] + '\n```'
+            role_out += f"\n```yml\n\'{item['RoleName']}\'\n - {item['Price']}\n```"
         embed.add_field(name='Items and Prices', value=role_out)
     await message.channel.send(None, embed=embed)
