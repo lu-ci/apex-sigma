93f457ffd693bb511c66f99335ee20fb5ecc2b1e
AXAz0r <thealeksaradovic@gmail.com>
2017-05-08 13:44:08 +0200

    Change WFLOC Title

--- sigma/plugins/games/warframe/wfloc.py
+++ sigma/plugins/games/warframe/wfloc.py
@@ -17,7 +17,7 @@ async def wfloc(cmd, message, args):
                 wiki_url = f'http://warframe.wikia.com/wiki/{qry}'
                 embed = await read_item_data(wiki_url)
                 icon_url = 'http://fc08.deviantart.net/fs70/f/2013/362/8/b/warframe_icon__alternative__by_bokuwatensai-d6zs6fz.png'
-                embed.set_author(name='Warframe Wikia', icon_url=icon_url, url=wiki_url)
+                embed.set_author(name='Prime Locator', icon_url=icon_url, url=wiki_url)
                 await message.channel.send(None, embed=embed)
             except:
                 embed = discord.Embed(color=0x696969, title='🔍 Nothing Found')
