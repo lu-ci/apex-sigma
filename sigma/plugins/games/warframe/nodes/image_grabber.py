7e271b5c1fece76519c9c872058d5f358502ab14
AXAz0r <thealeksaradovic@gmail.com>
2017-05-13 02:06:08 +0200

    Warframe Price Checking

--- sigma/plugins/games/warframe/nodes/image_grabber.py
+++ sigma/plugins/games/warframe/nodes/image_grabber.py
@@ -1,6 +1,7 @@
 import aiohttp
 import lxml.html as l
 
+
 async def grab_image(name):
     base_url = 'http://warframe.wikia.com/wiki'
     if ' Blueprint' in name:
@@ -22,3 +23,17 @@ async def grab_image(name):
     img_object = root.cssselect('.image')[0]
     img_url = img_object.attrib['href']
     return img_url
+
+
+async def alt_grab_image(name):
+    base_url = 'http://warframe.wikia.com/wiki'
+    if ' Blueprint' in name:
+        name = name.replace(' Blueprint', '')
+    name = name.replace(' ', '_')
+    item_url = f'{base_url}/{name}'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(item_url) as data:
+            page = await data.text()
+    root = l.fromstring(page)
+    item_image = root.cssselect('.infobox')[0][1][0][0].attrib['href']
+    return item_image
