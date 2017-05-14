56f08d572d70c7ba0177173434126d2e714529db
AXAz0r <thealeksaradovic@gmail.com>
2017-05-14 13:51:36 +0200

    Command and Module Permission Checks

--- sigma/core/blacklist.py
+++ sigma/core/blacklist.py
@@ -25,3 +25,56 @@ def check_black(db, message):
         if black_channel or black_user or server_is_black:
             black = True
     return black
+
+
+def check_overwrites(perms, author, channel, roles, cmd_name=None, mdl_name=None):
+    overwritten = False
+    if cmd_name:
+        broad_exceptions = perms['CommandExceptions']
+        trgt = cmd_name
+    elif mdl_name:
+        broad_exceptions = perms['ModuleExceptions']
+        trgt = mdl_name
+    else:
+        trgt = None
+        broad_exceptions = None
+    if broad_exceptions:
+        if trgt in broad_exceptions:
+            exceptions = broad_exceptions[trgt]
+            if author.id in exceptions['Users']:
+                overwritten = True
+            elif channel.id in exceptions['Channels']:
+                overwritten = True
+            for role in roles:
+                if role.id in exceptions['Roles']:
+                    overwritten = True
+                    break
+    return overwritten
+
+
+def check_perms(db, message, command):
+    if message.guild:
+        perms = db.find_one('Permissions', {'ServerID': message.guild.id})
+        if not perms:
+            permitted = True
+        else:
+            cmd = command.name
+            mdl = command.plugin.categories[0]
+            ath = message.author
+            chn = message.channel
+            rls = message.author.roles
+            if mdl in perms['DisabledModules']:
+                if check_overwrites(perms, ath, chn, rls, True):
+                    permitted = True
+                else:
+                    permitted = False
+            elif cmd in perms['DisabledCommands']:
+                if check_overwrites(perms, ath, chn, rls, False):
+                    permitted = True
+                else:
+                    permitted = False
+            else:
+                permitted = True
+    else:
+        permitted = True
+    return permitted
