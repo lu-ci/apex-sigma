719869518d05815849567b414c5eda032549ad64
AXAz0r <thealeksaradovic@gmail.com>
2017-05-26 06:40:26 +0200

    Custom Commands Abide By Blacklist

--- sigma/plugins/moderation/other/custom_command_detection.py
+++ sigma/plugins/moderation/other/custom_command_detection.py
@@ -1,4 +1,5 @@
 from config import Prefix
+from sigma.core.blacklist import check_black
 
 
 async def custom_command_detection(ev, message, args):
@@ -6,11 +7,12 @@ async def custom_command_detection(ev, message, args):
         if message.content.startswith(Prefix):
             cmd = message.content[len(Prefix):].lower()
             if cmd not in ev.bot.plugin_manager.commands:
-                try:
-                    custom_commands = ev.db.get_settings(message.guild.id, 'CustomCommands')
-                except:
-                    ev.db.set_settings(message.guild.id, 'CustomCommands', {})
-                    custom_commands = {}
-                if cmd in custom_commands:
-                    response = custom_commands[cmd]
-                    await message.channel.send(response)
+                if not check_black(ev.db, message):
+                    try:
+                        custom_commands = ev.db.get_settings(message.guild.id, 'CustomCommands')
+                    except:
+                        ev.db.set_settings(message.guild.id, 'CustomCommands', {})
+                        custom_commands = {}
+                    if cmd in custom_commands:
+                        response = custom_commands[cmd]
+                        await message.channel.send(response)
