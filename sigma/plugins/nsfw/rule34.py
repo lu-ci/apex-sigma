021e59caa253e8e7374e2e8a74685385e954abae
AXAz0r <thealeksaradovic@gmail.com>
2017-05-26 07:46:23 +0200

    R34 Rework

--- sigma/plugins/nsfw/rule34.py
+++ sigma/plugins/nsfw/rule34.py
@@ -11,15 +11,22 @@ async def rule34(cmd, message, args):
         if not tags:
             tags = 'nude'
 
-        r34_url = 'http://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=' + tags
+        r34_url = 'https://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=' + tags
         async with aiohttp.ClientSession() as session:
             async with session.get(r34_url) as data:
                 data = await data.read()
         posts = html.fromstring(data)
         choice = random.choice(posts)
-        url = str(choice.attrib['file_url']).replace('//', 'http://')
-        embed = discord.Embed(color=0x9933FF)
-        embed.set_image(url=url)
+        img_url = choice.attrib['file_url']
+        if not img_url.startswith('http'):
+            img_url = f"https:{choice.attrib['file_url']}"
+        icon_url = 'https://i.imgur.com/63GGrmG.png'
+        post_url = f'https://rule34.xxx/index.php?page=post&s=view&id={choice.attrib["id"]}'
+        embed = discord.Embed(color=0xaae5a3)
+        embed.set_author(name='Rule 34', url=post_url, icon_url=icon_url)
+        embed.set_image(url=img_url)
+        embed.set_footer(
+            text=f'Score: {choice.attrib["score"]} | Size: {choice.attrib["width"]}x{choice.attrib["height"]}')
         await message.channel.send(None, embed=embed)
     except Exception as e:
         cmd.log.info(e)
