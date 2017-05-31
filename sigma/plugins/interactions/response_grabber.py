b840d4bacbd75ed01200da0613fdfaae7f851aab
AXAz0r <thealeksaradovic@gmail.com>
2017-05-31 10:08:05 +0200

    Interaction Loading Changes

--- sigma/plugins/interactions/response_grabber.py
+++ sigma/plugins/interactions/response_grabber.py
@@ -1,9 +1,24 @@
 import yaml
 import random
 
+responses = {}
+
+
+def load_responses(file):
+    with open(file) as resp_file:
+        response_data = yaml.safe_load(resp_file)
+        return response_data
+
 
 def grab_response(file, trigger):
-    with open(file) as resp_file:
-        data = yaml.safe_load(resp_file)
-        resp = random.choice(data[trigger])
+    global responses
+    if not responses:
+        responses = load_responses(file)
+    if trigger not in responses:
+        resp_data = load_responses(file)
+        responses.update({trigger: resp_data[trigger]})
+    trigger_responses = responses[trigger]
+    random.shuffle(trigger_responses)
+    resp = trigger_responses.pop()
+    responses.update({trigger: trigger_responses})
     return resp
