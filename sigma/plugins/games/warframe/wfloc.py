1e8465c3569a398cc3d0dc436a7987cc41b94ebf
AXAz0r <thealeksaradovic@gmail.com>
2017-05-31 22:32:58 +0200

    Warframe Locator Fixes~

--- sigma/plugins/games/warframe/wfloc.py
+++ sigma/plugins/games/warframe/wfloc.py
@@ -5,15 +5,15 @@ from .nodes.read_frame_data import read_frame_data, read_item_data
 async def wfloc(cmd, message, args):
     if args:
         try:
-            qry = '/'.join(args).title()
+            qry = '/'.join(args).title().replace('And', '&')
             wiki_url = f'http://warframe.wikia.com/wiki/{qry}'
             embed = await read_frame_data(wiki_url)
             icon_url = 'http://fc08.deviantart.net/fs70/f/2013/362/8/b/warframe_icon__alternative__by_bokuwatensai-d6zs6fz.png'
-            embed.set_author(name='Warframe Wikia', icon_url=icon_url, url=wiki_url)
+            embed.set_author(name='Prime Locator', icon_url=icon_url, url=wiki_url)
             await message.channel.send(None, embed=embed)
         except:
             try:
-                qry = '_'.join(args).title()
+                qry = '_'.join(args).title().replace('And', '&')
                 wiki_url = f'http://warframe.wikia.com/wiki/{qry}'
                 embed = await read_item_data(wiki_url)
                 icon_url = 'http://fc08.deviantart.net/fs70/f/2013/362/8/b/warframe_icon__alternative__by_bokuwatensai-d6zs6fz.png'
