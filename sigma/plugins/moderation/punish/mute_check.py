a997fba474250f0d37d97996ea05ab0bb41f8f7a
AXAz0r <thealeksaradovic@gmail.com>
2017-06-03 12:51:02 +0200

    Check If Has Guild

--- sigma/plugins/moderation/punish/mute_check.py
+++ sigma/plugins/moderation/punish/mute_check.py
@@ -1,10 +1,11 @@
 async def mute_check(ev, message, args):
-    try:
-        mute_list = ev.db.get_settings(message.guild.id, 'MutedUsers')
-    except:
-        mute_list = []
-    if message.author.id in mute_list:
+    if message.guild:
         try:
-            await message.delete(reason='User is text muted.')
+            mute_list = ev.db.get_settings(message.guild.id, 'MutedUsers')
         except:
-            pass
+            mute_list = []
+        if message.author.id in mute_list:
+            try:
+                await message.delete(reason='User is text muted.')
+            except:
+                pass
