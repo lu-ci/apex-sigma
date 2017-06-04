7facbdb1eb4c131252ef44971bc43b0eef1c4b38
AXAz0r <thealeksaradovic@gmail.com>
2017-06-04 07:31:46 +0200

    Prune Logging

--- sigma/plugins/moderation/channels/prune.py
+++ sigma/plugins/moderation/channels/prune.py
@@ -1,6 +1,8 @@
-﻿import discord
+﻿import arrow
+import discord
 import asyncio
 from sigma.core.permission import check_man_msg
+from sigma.core.utils import user_avatar
 
 
 async def prune(cmd, message, args):
@@ -33,6 +35,27 @@ async def prune(cmd, message, args):
         else:
             deleted = await message.channel.purge(limit=limit)
         embed = discord.Embed(color=0x66CC66, title=f'✅ Deleted {len(deleted)} Messages')
+        try:
+            log_channel_id = cmd.db.get_settings(message.guild.id, 'LoggingChannel')
+        except:
+            log_channel_id = None
+        if log_channel_id:
+            log_channel = discord.utils.find(lambda x: x.id == log_channel_id, message.guild.channels)
+            if log_channel:
+                response = discord.Embed(color=0x696969, timestamp=arrow.utcnow().datetime)
+                response.set_author(name=f'#{channel.name} Has Been Pruned', icon_url=user_avatar(message.author))
+                if target:
+                    target_text = f'{target.mention}'
+                else:
+                    target_text = 'No Filter'
+                response.add_field(name='🗑 Prune Details',
+                                   value=f'Amount: {len(deleted)} Messages\nTarget: {target_text}')
+                author = message.author
+                response.add_field(name='🛡 Responsible',
+                                   value=f'{author.mention}\n{author.name}#{author.discriminator}',
+                                   inline=True)
+                response.set_footer(text=f'ChannelID: {channel.id}')
+                await log_channel.send(embed=response)
     else:
         embed = discord.Embed(title='⚠ Unpermitted. Only Those With The Manage Message Permission Allowed.',
                               color=0xDB0000)
