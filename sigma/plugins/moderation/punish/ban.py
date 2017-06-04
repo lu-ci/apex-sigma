687382750769fb2a92b63501012d1c4ed0345805
AXAz0r <thealeksaradovic@gmail.com>
2017-06-04 07:52:28 +0200

    Ban Logging

--- sigma/plugins/moderation/punish/ban.py
+++ sigma/plugins/moderation/punish/ban.py
@@ -1,5 +1,7 @@
 ﻿from sigma.core.permission import check_ban
+from sigma.core.utils import user_avatar
 import discord
+import arrow
 
 
 async def ban(cmd, message, args):
@@ -8,11 +10,36 @@ async def ban(cmd, message, args):
     else:
         if message.mentions:
             target = message.mentions[0]
+
             if target.id == message.author.id:
                 response = discord.Embed(title='⛔ You can\'t ban yourself.', color=0xDB0000)
             else:
-                await target.ban(reason=f'Banned by {message.author.name}#{message.author.discriminator}.')
+                if len(args) > 1:
+                    ban_reason = 'No reason given.'
+                else:
+                    ban_reason = ' '.join(args[1:])
+                await target.ban(reason=f'Banned by {message.author.name}#{message.author.discriminator}.\n{ban_reason}')
                 response = discord.Embed(title=f'🔨 {target.name} has been banned.', color=0x993300)
+                # Logging Part
+                try:
+                    log_channel_id = cmd.db.get_settings(message.guild.id, 'LoggingChannel')
+                except:
+                    log_channel_id = None
+                if log_channel_id:
+                    log_channel = discord.utils.find(lambda x: x.id == log_channel_id, message.guild.channels)
+                    if log_channel:
+                        response = discord.Embed(color=0x993300, timestamp=arrow.utcnow().datetime)
+                        response.set_author(name=f'A User Has Been Banned', icon_url=user_avatar(target))
+                        response.add_field(name='🔨 Banned User',
+                                           value=f'{target.mention}\n{target.name}#{target.discriminator}', inline=True)
+                        author = message.author
+                        response.add_field(name='🛡 Responsible',
+                                           value=f'{author.mention}\n{author.name}#{author.discriminator}',
+                                           inline=True)
+                        response.add_field(name='📄 Reason', value=f"```\n{ban_reason}\n```", inline=False)
+                        response.set_footer(text=f'UserID: {target.id}')
+                        await log_channel.send(embed=response)
+
         else:
             response = discord.Embed(title='❗ No user targeted.', color=0xDB0000)
     await message.channel.send(embed=response)
