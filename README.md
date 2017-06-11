087c4f2194f1324fb710d091a408c8c50ff8be49
AXAz0r <thealeksaradovic@gmail.com>
2017-06-12 00:29:23 +0200

    Restored Kud Earning

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 [![apex](https://i.imgur.com/TRSdGni.png)](https://auroraproject.xyz/)
 
 <p align="center">
-<img src="https://img.shields.io/badge/Sigma-2.90-1B6F5F.svg?style=flat-square" />
+<img src="https://img.shields.io/badge/Sigma-2.92-1B6F5F.svg?style=flat-square" />
 <img src="https://img.shields.io/badge/Codename-Minagi-1B6F5F.svg?style=flat-square" />
 <img src="https://img.shields.io/badge/License-GPLv3-red.svg?style=flat-square" />
 </p>
---
+++ sigma/plugins/points/reward.py
@@ -0,0 +1,12 @@
+﻿import random
+from config import Prefix
+
+
+async def reward(ev, message, args):
+    if not message.author.bot:
+        if message.guild:
+            if not message.content.startswith(Prefix):
+                if not ev.cooldown.on_cooldown(ev, message):
+                    points = random.randint(3, 15)
+                    ev.db.add_points(message.guild, message.author, points)
+                    ev.cooldown.set_cooldown(ev, message, 60)
