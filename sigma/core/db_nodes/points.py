8d40a49820b7cd769cced7b30111c10c5429f313
AXAz0r <thealeksaradovic@gmail.com>
2017-06-12 12:46:31 +0200

    Better Point Manipulation

--- sigma/core/db_nodes/points.py
+++ sigma/core/db_nodes/points.py
@@ -18,28 +18,26 @@ def point_manipulation(db, server, user, points, add):
     collection = 'PointSystem'
     sid = str(server.id)
     data = db[collection].find_one({'UserID': user.id})
-    if data:
-        total_pts = data['Total']
-        servers = data['Servers']
-        cur_pts = data['Current']
-        if sid in servers:
-            if add:
-                srv_pts = servers[sid] + points
-                total_pts += points
-            else:
-                srv_pts = servers[sid]
-        else:
-            srv_pts = points
-        if add:
-            cur_pts += points
-        else:
-            cur_pts -= points
-        servers.update({sid: srv_pts})
-        db[collection].update_one({'UserID': user.id},
-                                  {'$set': {'Servers': servers, 'Total': total_pts, 'Current': cur_pts}})
-    else:
+    if not data:
         data = default_data(server, user, points, add)
         db[collection].insert_one(data)
+        data = db[collection].find_one({'UserID': user.id})
+    total_pts = data['Total']
+    servers = data['Servers']
+    cur_pts = data['Current']
+    if sid in servers:
+        srv_pts = servers[sid]
+    else:
+        srv_pts = 0
+    if add:
+        total_pts += points
+        srv_pts += points
+        cur_pts += points
+    else:
+        cur_pts -= points
+    servers.update({sid: srv_pts})
+    db[collection].update_one({'UserID': user.id},
+                              {'$set': {'Servers': servers, 'Total': total_pts, 'Current': cur_pts}})
 
 
 def point_grabber(db, user):
