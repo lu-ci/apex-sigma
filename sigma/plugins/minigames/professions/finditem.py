0a704b61c62e7af38ce1f3cee89aaab0c211cbff
AXAz0r <thealeksaradovic@gmail.com>
2017-06-20 04:22:27 +0200

    FindItem Command

---
+++ sigma/plugins/minigames/professions/finditem.py
@@ -0,0 +1,37 @@
+import discord
+from config import Currency
+from .mechanics import get_all_items
+
+async def finditem(cmd, message, args):
+    if args:
+        if len(args) >= 2:
+            item_type = args[0].lower()
+            lookup = ' '.join(args[1:])
+            try:
+                inv = get_all_items(item_type, cmd.resource('data'))
+            except KeyError:
+                inv = None
+            if inv:
+                item = None
+                for invitem in inv:
+                    if inv[invitem].name.lower() == lookup.lower():
+                        item = inv[invitem]
+                        break
+                if item:
+                    connector = 'A'
+                    if item.rarity_name[0].lower() in ['a', 'e', 'i', 'o', 'u']:
+                        connector = 'An'
+                    item_info = f'{connector} **{item.rarity_name.title()} {item.item_type.title()}**'
+                    item_info += f'\nIt is valued at **{item.value} {Currency}**'
+                    response = discord.Embed(color=item.color)
+                    response.add_field(name=f'{item.icon} {item.name}', value=f'{item_info}')
+                    response.add_field(name='Item Description', value=f'{item.description}', inline=False)
+                else:
+                    response = discord.Embed(color=0x696969, title=f'🔍 I didn\'t find any {lookup} in your inventory.')
+            else:
+                response = discord.Embed(color=0x696969, title=f'🔍 I didn\'t find the {item_type} category.')
+        else:
+            response = discord.Embed(color=0xDB0000, title='❗ Not enough arguments..')
+    else:
+        response = discord.Embed(color=0xDB0000, title='❗ You didn\'t input anything.')
+    await message.channel.send(embed=response)
