88128658d7f08b413ae0c811bb6365e956b14875
AXAz0r <thealeksaradovic@gmail.com>
2017-07-02 13:00:31 +0200

    Dan's Smitten Command

---
+++ sigma/plugins/special/smitten.py
@@ -0,0 +1,18 @@
+import discord
+
+
+async def smitten(cmd, message, args):
+    dan_id = 285232223127601152
+    dawn_id = 222234484064518156
+    if message.author.id == dan_id:
+        target_id = dawn_id
+    elif message.author.id == dawn_id:
+        target_id = dan_id
+    else:
+        return
+    target = discord.utils.find(lambda x: x.id == target_id, cmd.bot.get_all_members())
+    if target:
+        img_url = 'https://i.imgur.com/Kj9x7Az.gif'
+        response = discord.Embed(color=0xff6699, title='💝 Dan x Dawn')
+        response.set_image(url=img_url)
+        await target.send(embed=response)
