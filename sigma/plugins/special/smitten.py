e817883f660ab6de17d1e19b3835543ee6bd590e
AXAz0r <thealeksaradovic@gmail.com>
2017-07-02 13:07:11 +0200

    A small adjustment

--- sigma/plugins/special/smitten.py
+++ sigma/plugins/special/smitten.py
@@ -1,5 +1,5 @@
 import discord
-
+import secrets
 
 async def smitten(cmd, message, args):
     dan_id = 285232223127601152
@@ -12,7 +12,11 @@ async def smitten(cmd, message, args):
         return
     target = discord.utils.find(lambda x: x.id == target_id, cmd.bot.get_all_members())
     if target:
-        img_url = 'https://i.imgur.com/Kj9x7Az.gif'
+        url_list = [
+            'https://i.imgur.com/HQHsDOY.gif',
+            'https://i.imgur.com/Kj9x7Az.gif'
+        ]
+        img_url = secrets.choice(url_list)
         response = discord.Embed(color=0xff6699, title='💝 Dan x Dawn')
         response.set_image(url=img_url)
         await target.send(embed=response)
